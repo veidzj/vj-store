1ea85dbed8146cc45f917a1eea18579ab2a9b57c
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-10-22 12:24:28 -0300

    refactor: add faker to remove duplicated mock code

--- tests/presentation/controllers/sign-up-controller.spec.ts
+++ tests/presentation/controllers/sign-up-controller.spec.ts
@@ -1,3 +1,4 @@
+import { faker } from '@faker-js/faker'
 import { ValidationSpy } from '../mocks/mock-validation'
 import { SignUpController } from '../../../src/presentation/controllers/sign-up-controller'
 import { MissingParamError } from '../../../src/presentation/errors/missing-param-error'
@@ -16,15 +17,20 @@ const makeSut = (): Sut => {
   }
 }
 
+const mockRequest = (): any => {
+  const password = faker.internet.password()
+  return {
+    name: faker.person.firstName(),
+    email: faker.internet.email(),
+    password,
+    passwordConfirmation: password
+  }
+}
+
 describe('SignUpController', () => {
   test('Should call Validation with correct values', async() => {
     const { sut, validationSpy } = makeSut()
-    const request = {
-      name: '',
-      email: '',
-      password: '',
-      passwordConfirmation: ''
-    }
+    const request = mockRequest()
     await sut.handle(request)
     expect(validationSpy.input).toEqual(request)
   })
@@ -32,13 +38,7 @@ describe('SignUpController', () => {
   test('Should return Bad Request if Validation returns an error', async() => {
     const { sut, validationSpy } = makeSut()
     validationSpy.error = new MissingParamError('name')
-    const request = {
-      name: '',
-      email: '',
-      password: '',
-      passwordConfirmation: ''
-    }
-    const httpResponse = await sut.handle(request)
+    const httpResponse = await sut.handle(mockRequest())
     expect(httpResponse).toEqual({
       statusCode: 400,
       body: new MissingParamError('name')
