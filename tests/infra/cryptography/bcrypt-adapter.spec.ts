0944eadbd68ca0eddb7544bbdccecf18873b52d2
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-10-24 10:10:45 -0300

    test: ensure BcryptAdapter throws if hash throws

--- tests/infra/cryptography/bcrypt-adapter.spec.ts
+++ tests/infra/cryptography/bcrypt-adapter.spec.ts
@@ -1,5 +1,6 @@
 import bcrypt from 'bcrypt'
 import { BcryptAdapter } from '../../../src/infra/cryptography/bcrypt-adapter'
+import { throwError } from '../../domain/mocks/test-helper'
 
 jest.mock('bcrypt', () => ({
   async hash(): Promise<string> {
@@ -20,6 +21,13 @@ describe('BcryptAdapter', () => {
       const hashSpy = jest.spyOn(bcrypt, 'hash')
       await sut.hash('any_value')
       expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
+    })
+
+    test('Should throw if hash throws', async() => {
+      const sut = makeSut()
+      jest.spyOn(bcrypt, 'hash').mockImplementationOnce(throwError)
+      const promise = sut.hash('any_value')
+      await expect(promise).rejects.toThrow()
     })
   })
 })
