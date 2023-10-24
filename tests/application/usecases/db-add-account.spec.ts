e57486debb6e7bd9bd95504701463abdf98385c5
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-10-24 02:59:25 -0300

    test: ensure DbAddAccount throws if CheckAccountByEmailRepository throws

--- tests/application/usecases/db-add-account.spec.ts
+++ tests/application/usecases/db-add-account.spec.ts
@@ -1,5 +1,6 @@
 import { DbAddAccount } from '../../../src/application/usecases/db-add-account'
 import { mockAddAccountInput } from '../../domain/mocks/mock-account'
+import { throwError } from '../../domain/mocks/test-helper'
 import { HasherSpy } from '../mocks/mock-cryptography'
 import { AddAccountRepositorySpy, CheckAccountByEmailRepositorySpy } from '../mocks/mock-db-account'
 
@@ -24,10 +25,19 @@ const makeSut = (): Sut => {
 }
 
 describe('DbAddAccount', () => {
-  test('Should call CheckAccountByEmailRepository with correct email', async() => {
-    const { sut, checkAccountByEmailRepositorySpy } = makeSut()
-    const addAccountInput = mockAddAccountInput()
-    await sut.add(addAccountInput)
-    expect(checkAccountByEmailRepositorySpy.input).toEqual(addAccountInput.email)
+  describe('CheckAccountByEmailRepository', () => {
+    test('Should call CheckAccountByEmailRepository with correct email', async() => {
+      const { sut, checkAccountByEmailRepositorySpy } = makeSut()
+      const addAccountInput = mockAddAccountInput()
+      await sut.add(addAccountInput)
+      expect(checkAccountByEmailRepositorySpy.input).toEqual(addAccountInput.email)
+    })
+
+    test('Should throw if CheckAccountByEmailRepository throws', async() => {
+      const { sut, checkAccountByEmailRepositorySpy } = makeSut()
+      jest.spyOn(checkAccountByEmailRepositorySpy, 'checkByEmail').mockImplementationOnce(throwError)
+      const promise = sut.add(mockAddAccountInput())
+      await expect(promise).rejects.toThrow()
+    })
   })
 })
