48536a4af28b5193998afcc731816bf19e1b7ce4
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-10-24 07:41:55 -0300

    test: ensure EmailValidatorAdapter returns true if validator returns true

--- tests/infra/validators/email-validator-adapter.spec.ts
+++ tests/infra/validators/email-validator-adapter.spec.ts
@@ -22,7 +22,13 @@ describe('EmailValidatorAdapter', () => {
   test('Should return false if validator returns false', () => {
     const sut = makeSut()
     jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
-    const isValid = sut.isValid('valid_email@mail.com')
+    const isValid = sut.isValid('invalid_email@mail.com')
     expect(isValid).toBe(false)
+  })
+
+  test('Should return true if validator returns true', () => {
+    const sut = makeSut()
+    const isValid = sut.isValid('valid_email@mail.com')
+    expect(isValid).toBe(true)
   })
 })
