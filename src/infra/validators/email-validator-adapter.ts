73e80b7f305be234a0629e797e51128a4f3e7b18
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-10-24 06:22:34 -0300

    feat: ensure EmailValidatorAdapter calls validator with correct email

---
+++ src/infra/validators/email-validator-adapter.ts
@@ -0,0 +1,8 @@
+import validator from 'validator'
+import { type EmailValidator } from '../../validation/protocols/email-validator'
+
+export class EmailValidatorAdapter implements EmailValidator {
+  public isValid = (email: string): boolean => {
+    return validator.isEmail(email)
+  }
+}
