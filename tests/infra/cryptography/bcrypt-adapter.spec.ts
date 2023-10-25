95e953293f34323aa3f41d8a11f55f4f3531f584
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-10-25 12:44:45 -0300

    test: ensure BcryptAdapter throws if compare throws

--- tests/infra/cryptography/bcrypt-adapter.spec.ts
+++ tests/infra/cryptography/bcrypt-adapter.spec.ts
@@ -48,5 +48,12 @@ describe('BcryptAdapter', () => {
       await sut.compare('any_value', 'any_hash')
       expect(hashSpy).toHaveBeenCalledWith('any_value', 'any_hash')
     })
+
+    test('Should throw if compare throws', async() => {
+      const sut = makeSut()
+      jest.spyOn(bcrypt, 'compare').mockImplementationOnce(throwError)
+      const promise = sut.compare('any_value', 'any_hash')
+      await expect(promise).rejects.toThrow()
+    })
   })
 })
