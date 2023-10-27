10d5ab74ed44a0ecc6ca15547450a6f5a0944c7e
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-10-27 18:05:07 -0300

    feat: ensure AccountMongoRepository checkByEmail() returns true if email exists

--- src/infra/db/mongodb/account-mongo-repository.ts
+++ src/infra/db/mongodb/account-mongo-repository.ts
@@ -1,8 +1,21 @@
+import { type CheckAccountByEmailRepository } from '../../../application/protocols/db/check-account-by-email-repository'
 import { type AddAccountRepository } from '../../../application/protocols/db/add-account-repository'
 import { type AddAccount } from '../../../domain/usecases/add-account'
 import { MongoHelper } from './mongo-helper'
 
-export class AccountMongoRepository implements AddAccountRepository {
+export class AccountMongoRepository implements CheckAccountByEmailRepository, AddAccountRepository {
+  public checkByEmail = async(input: string): Promise<boolean> => {
+    const accountCollection = MongoHelper.getCollection('account')
+    const account = await accountCollection.findOne({
+      email: input
+    }, {
+      projection: {
+        _id: 1
+      }
+    })
+    return account !== null
+  }
+
   public add = async(input: AddAccount.Input): Promise<boolean> => {
     const accountCollection = MongoHelper.getCollection('account')
     const result = await accountCollection.insertOne(input)
