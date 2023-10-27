20cd071b88eb3d337a91ae9923aceda28cd9c9b0
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-10-27 18:18:08 -0300

    feat: ensure AccountMongoRepository getByEmail() returns an account if email exists

--- src/infra/db/mongodb/account-mongo-repository.ts
+++ src/infra/db/mongodb/account-mongo-repository.ts
@@ -1,9 +1,10 @@
 import { type CheckAccountByEmailRepository } from '../../../application/protocols/db/check-account-by-email-repository'
 import { type AddAccountRepository } from '../../../application/protocols/db/add-account-repository'
+import { type GetAccountByEmailRepository } from '../../../application/protocols/db/get-account-by-email-repository'
 import { type AddAccount } from '../../../domain/usecases/add-account'
 import { MongoHelper } from './mongo-helper'
 
-export class AccountMongoRepository implements CheckAccountByEmailRepository, AddAccountRepository {
+export class AccountMongoRepository implements CheckAccountByEmailRepository, AddAccountRepository, GetAccountByEmailRepository {
   public checkByEmail = async(email: string): Promise<boolean> => {
     const accountCollection = MongoHelper.getCollection('account')
     const account = await accountCollection.findOne({
@@ -20,5 +21,19 @@ export class AccountMongoRepository implements CheckAccountByEmailRepository, Ad
     const accountCollection = MongoHelper.getCollection('account')
     const result = await accountCollection.insertOne(input)
     return result.insertedId !== null
+  }
+
+  public getByEmail = async(email: string): Promise<GetAccountByEmailRepository.Output | null> => {
+    const accountCollection = MongoHelper.getCollection('account')
+    const account = await accountCollection.findOne({
+      email
+    }, {
+      projection: {
+        _id: 1,
+        name: 1,
+        password: 1
+      }
+    })
+    return account && MongoHelper.map(account)
   }
 }
