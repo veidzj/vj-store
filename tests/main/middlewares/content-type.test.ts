adb2f83607241a34864e4ee0f0e7bf1e97e5a77d
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-10-28 08:57:49 -0300

    test: ensure routes returns xml content type when forced

--- tests/main/middlewares/content-type.test.ts
+++ tests/main/middlewares/content-type.test.ts
@@ -17,4 +17,14 @@ describe('ContentType Middleware', () => {
       .get('/test_content_type')
       .expect('content-type', /json/)
   })
+
+  test('Should return xml content type when forced', async() => {
+    app.get('/test_content_type_xml', (req, res) => {
+      res.type('xml')
+      res.send()
+    })
+    await request(app)
+      .get('/test_content_type_xml')
+      .expect('content-type', /xml/)
+  })
 })
