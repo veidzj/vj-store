988b41a1344400eabf8c84abe988b0a248007794
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-10-28 14:07:06 -0300

    test: ensure ValidationComposite returns the first error if more than one validation fails

--- tests/validation/validators/validation-composite.spec.ts
+++ tests/validation/validators/validation-composite.spec.ts
@@ -29,4 +29,12 @@ describe('ValidationComposite', () => {
     const error = sut.validate({ [field]: faker.word.words() })
     expect(error).toEqual(validationSpies[1].error)
   })
+
+  test('Should return the first error if more than one validation fails', () => {
+    const { sut, validationSpies } = makeSut()
+    validationSpies[0].error = new Error()
+    validationSpies[1].error = new MissingParamError(field)
+    const error = sut.validate({ [field]: faker.word.words() })
+    expect(error).toEqual(validationSpies[0].error)
+  })
 })
