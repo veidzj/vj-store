a7613941a64290ed88e5bca8015f862277110938
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-10-28 08:56:50 -0300

    feat: ensure routes returns default content type as json

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,6 +1,8 @@
 import { type Express } from 'express'
-import { bodyParser } from '../middlewares/body-parser'
+import { bodyParser } from '@/main/middlewares/body-parser'
+import { contentType } from '@/main/middlewares/content-type'
 
 export default (app: Express): void => {
   app.use(bodyParser)
+  app.use(contentType)
 }
