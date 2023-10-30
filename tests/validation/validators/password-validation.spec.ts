4a2b7849e182c4e4ac3ef72ae08ddab90984bf74
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-10-30 20:49:22 -0300

    fix: correct params to PasswordValidation tests

--- tests/validation/validators/password-validation.spec.ts
+++ tests/validation/validators/password-validation.spec.ts
@@ -2,7 +2,7 @@ import { faker } from '@faker-js/faker'
 import { PasswordValidation } from '@/validation/validators/password-validation'
 import { InvalidParamError } from '@/presentation/errors/invalid-param-error'
 
-const field = faker.internet.password()
+const field = faker.word.words()
 
 const makeSut = (): PasswordValidation => {
   return new PasswordValidation(field)
@@ -11,13 +11,15 @@ const makeSut = (): PasswordValidation => {
 describe('PasswordValidation', () => {
   test('Should return InvalidParamError if validation fails', () => {
     const sut = makeSut()
-    const error = sut.validate(faker.internet.password({ length: 5 }))
+    const password = faker.internet.password({ length: 5 })
+    const error = sut.validate({ [field]: password })
     expect(error).toEqual(new InvalidParamError(field, 'must be at least 6 characters long'))
   })
 
   test('Should return null if validation succeeds', () => {
     const sut = makeSut()
-    const error = sut.validate(faker.internet.password())
+    const password = faker.internet.password()
+    const error = sut.validate({ [field]: password })
     expect(error).toBeNull()
   })
 })
