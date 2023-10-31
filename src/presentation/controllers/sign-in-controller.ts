0ee7f0ad732c4ff7d5fcb6383763c7a1f56f5ee4
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-10-31 20:59:18 -0300

    feat: ensure SignInController returns Bad Request if Validation returns an error

--- src/presentation/controllers/sign-in-controller.ts
+++ src/presentation/controllers/sign-in-controller.ts
@@ -1,12 +1,16 @@
 import { type Controller } from '@/presentation/protocols/controller'
 import { type Validation } from '@/presentation/protocols/validation'
 import { type HttpResponse } from '@/presentation/protocols/http'
+import { badRequest } from '@/presentation/helpers/http-helper'
 
 export class SignInController implements Controller {
   constructor(private readonly validation: Validation) {}
 
   public handle = async(request: any): Promise<HttpResponse> => {
-    this.validation.validate(request)
+    const error = this.validation.validate(request)
+    if (error) {
+      return badRequest(error)
+    }
     return {
       statusCode: 400,
       body: ''
