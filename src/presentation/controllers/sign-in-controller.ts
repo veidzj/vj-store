2b351abe2429b216c20c299a357eee7472774ddd
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-10-31 21:04:38 -0300

    feat: ensure SignInController calls Authentication with correct values

--- src/presentation/controllers/sign-in-controller.ts
+++ src/presentation/controllers/sign-in-controller.ts
@@ -1,16 +1,22 @@
 import { type Controller } from '@/presentation/protocols/controller'
 import { type Validation } from '@/presentation/protocols/validation'
+import { type Authentication } from '@/domain/usecases/authentication'
 import { type HttpResponse } from '@/presentation/protocols/http'
 import { badRequest } from '@/presentation/helpers/http-helper'
 
 export class SignInController implements Controller {
-  constructor(private readonly validation: Validation) {}
+  constructor(
+    private readonly validation: Validation,
+    private readonly authentication: Authentication
+  ) {}
 
   public handle = async(request: any): Promise<HttpResponse> => {
     const error = this.validation.validate(request)
     if (error) {
       return badRequest(error)
     }
+
+    await this.authentication.auth(request)
     return {
       statusCode: 400,
       body: ''
