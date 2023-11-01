4b714f7eca2b04f34d91b7b411ef4e05ad2e22af
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-10-31 21:04:53 -0300

    test: ensure SignInController calls Authentication with correct values

--- tests/presentation/controllers/sign-in-controller.spec.ts
+++ tests/presentation/controllers/sign-in-controller.spec.ts
@@ -3,18 +3,22 @@ import { SignInController } from '@/presentation/controllers/sign-in-controller'
 import { ValidationSpy } from '@/tests/presentation/mocks/mock-validation'
 import { MissingParamError } from '@/presentation/errors/missing-param-error'
 import { badRequest } from '@/presentation/helpers/http-helper'
+import { AuthenticationSpy } from '@/tests/presentation/mocks/mock-account'
 
 interface Sut {
   sut: SignInController
   validationSpy: ValidationSpy
+  authenticationSpy: AuthenticationSpy
 }
 
 const makeSut = (): Sut => {
   const validationSpy = new ValidationSpy()
-  const sut = new SignInController(validationSpy)
+  const authenticationSpy = new AuthenticationSpy()
+  const sut = new SignInController(validationSpy, authenticationSpy)
   return {
     sut,
-    validationSpy
+    validationSpy,
+    authenticationSpy
   }
 }
 
@@ -38,6 +42,15 @@ describe('SignInController', () => {
       const request = mockRequest()
       const httpResponse = await sut.handle(request)
       expect(httpResponse).toEqual(badRequest(validationSpy.error))
+    })
+  })
+
+  describe('Authentication', () => {
+    test('Should call Authentication with correct values', async() => {
+      const { sut, authenticationSpy } = makeSut()
+      const request = mockRequest()
+      await sut.handle(request)
+      expect(authenticationSpy.input).toEqual(request)
     })
   })
 })
