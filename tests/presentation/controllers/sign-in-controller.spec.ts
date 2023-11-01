2d555f3426dfe9b39ddf1ffdf3930a89569ba50e
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-10-31 21:00:04 -0300

    test: ensure SignInController returns Bad Request if Validation returns an error

--- tests/presentation/controllers/sign-in-controller.spec.ts
+++ tests/presentation/controllers/sign-in-controller.spec.ts
@@ -1,6 +1,8 @@
 import { faker } from '@faker-js/faker'
 import { SignInController } from '@/presentation/controllers/sign-in-controller'
 import { ValidationSpy } from '@/tests/presentation/mocks/mock-validation'
+import { MissingParamError } from '@/presentation/errors/missing-param-error'
+import { badRequest } from '@/presentation/helpers/http-helper'
 
 interface Sut {
   sut: SignInController
@@ -28,6 +30,14 @@ describe('SignInController', () => {
       const request = mockRequest()
       await sut.handle(request)
       expect(validationSpy.input).toEqual(request)
+    })
+
+    test('Should return Bad Request if Validation returns an error', async() => {
+      const { sut, validationSpy } = makeSut()
+      validationSpy.error = new MissingParamError(faker.word.words())
+      const request = mockRequest()
+      const httpResponse = await sut.handle(request)
+      expect(httpResponse).toEqual(badRequest(validationSpy.error))
     })
   })
 })
