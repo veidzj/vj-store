068a5af0eff9f9535879841b81e5eded3eba90d0
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-10-31 21:16:53 -0300

    feat: ensure SignInController returns Server Error if Authentication throws

--- src/presentation/controllers/sign-in-controller.ts
+++ src/presentation/controllers/sign-in-controller.ts
@@ -2,7 +2,7 @@ import { type Controller } from '@/presentation/protocols/controller'
 import { type Validation } from '@/presentation/protocols/validation'
 import { type Authentication } from '@/domain/usecases/authentication'
 import { type HttpResponse } from '@/presentation/protocols/http'
-import { badRequest, ok, unauthorized } from '@/presentation/helpers/http-helper'
+import { badRequest, unauthorized, ok, serverError } from '@/presentation/helpers/http-helper'
 
 export class SignInController implements Controller {
   constructor(
@@ -11,15 +11,19 @@ export class SignInController implements Controller {
   ) {}
 
   public handle = async(request: any): Promise<HttpResponse> => {
-    const error = this.validation.validate(request)
-    if (error) {
-      return badRequest(error)
-    }
+    try {
+      const error = this.validation.validate(request)
+      if (error) {
+        return badRequest(error)
+      }
 
-    const authenticationModel = await this.authentication.auth(request)
-    if (!authenticationModel) {
-      return unauthorized()
+      const authenticationModel = await this.authentication.auth(request)
+      if (!authenticationModel) {
+        return unauthorized()
+      }
+      return ok(authenticationModel)
+    } catch (error) {
+      return serverError(error)
     }
-    return ok(authenticationModel)
   }
 }
