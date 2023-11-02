72607ed4984a1445ccfd76bd14d40429382c37af
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-11-02 20:26:31 -0300

    feat: ensure updateAccessToken throws if mongo throws

---
+++ src/application/protocols/db/dynamic/authentication/update-access-token-repository.ts
@@ -0,0 +1,10 @@
+export interface UpdateAccessTokenRepository {
+  updateAccessToken: (input: UpdateAccessTokenRepository.Input) => Promise<void>
+}
+
+export namespace UpdateAccessTokenRepository {
+  export interface Input {
+    id: string
+    token: string
+  }
+}
