2f4edc760f643c679c6a2f5b04b5cd56e55a4834
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-11-04 22:07:53 -0300

    feat: ensure DbGetAccountByToken calls GetAccountByTokenRepository with correct values

--- src/application/usecases/authentication/db-get-account-by-token.ts
+++ src/application/usecases/authentication/db-get-account-by-token.ts
@@ -1,14 +1,17 @@
 import { type Decrypter } from '@/application/protocols/cryptography'
+import { type GetAccountByTokenRepository } from '@/application/protocols/db/static/authentication'
 import { type GetAccountByToken } from '@/domain/usecases/authentication'
 
 export class DbGetAccountByToken implements GetAccountByToken {
   constructor(
-    private readonly decrypter: Decrypter
+    private readonly decrypter: Decrypter,
+    private readonly getAccountByTokenRepository: GetAccountByTokenRepository
   ) {}
 
   public getByToken = async(accessToken: string, role?: string): Promise<string | null> => {
     const token = await this.decrypter.decrypt(accessToken)
     if (!token) return null
+    await this.getAccountByTokenRepository.getByToken(accessToken, role)
     return ''
   }
 }
