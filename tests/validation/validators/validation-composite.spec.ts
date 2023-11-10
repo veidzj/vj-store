e52100594f92ea009969ecb98f19c484e0a41ecf
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-11-10 00:11:03 -0300

    fix: tests for ValidationComposite

--- tests/validation/validators/validation-composite.spec.ts
+++ tests/validation/validators/validation-composite.spec.ts
@@ -25,21 +25,27 @@ const makeSut = (): Sut => {
 describe('ValidationComposite', () => {
   test('Should throw an error if any validation fails', () => {
     const { sut, validationSpies } = makeSut()
-    validationSpies[1].error = new MissingParamError(field)
+    validationSpies[1].validate = jest.fn(() => {
+      throw new MissingParamError(field)
+    })
     const error = (): void => {
       sut.validate({ [field]: faker.word.words() })
     }
-    expect(error).toThrow(validationSpies[1].error)
+    expect(error).toThrow(new MissingParamError(field))
   })
 
   test('Should thow the first error if more than one validation fails', () => {
     const { sut, validationSpies } = makeSut()
-    validationSpies[0].error = new Error()
-    validationSpies[1].error = new MissingParamError(field)
+    validationSpies[0].validate = jest.fn(() => {
+      throw new Error()
+    })
+    validationSpies[1].validate = jest.fn(() => {
+      throw new MissingParamError(field)
+    })
     const error = (): void => {
       sut.validate({ [field]: faker.word.words() })
     }
-    expect(error).toThrow(validationSpies[0].error)
+    expect(error).toThrow(new Error())
   })
 
   test('Should not throw if validation succeeds', () => {
