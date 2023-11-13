a24fbc2bd350d078dd13e0707da4c5c8d9eb4c82
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-11-13 11:18:49 -0300

    refactor: use faker in EmailValidatorAdapter tests

--- tests/infra/validators/email-validator-adapter.spec.ts
+++ tests/infra/validators/email-validator-adapter.spec.ts
@@ -1,3 +1,4 @@
+import { faker } from '@faker-js/faker'
 import validator from 'validator'
 import { EmailValidatorAdapter } from '@/infra/validators'
 
@@ -15,20 +16,21 @@ describe('EmailValidatorAdapter', () => {
   test('Should call validator with correct email', () => {
     const sut = makeSut()
     const isEmailSpy = jest.spyOn(validator, 'isEmail')
-    sut.isValid('any_email@mail.com')
-    expect(isEmailSpy).toHaveBeenCalledWith('any_email@mail.com')
+    const email = faker.internet.email()
+    sut.isValid(email)
+    expect(isEmailSpy).toHaveBeenCalledWith(email)
   })
 
   test('Should return false if validator returns false', () => {
     const sut = makeSut()
     jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
-    const isValid = sut.isValid('invalid_email@mail.com')
+    const isValid = sut.isValid(faker.internet.email())
     expect(isValid).toBe(false)
   })
 
   test('Should return true if validator returns true', () => {
     const sut = makeSut()
-    const isValid = sut.isValid('valid_email@mail.com')
+    const isValid = sut.isValid(faker.internet.email())
     expect(isValid).toBe(true)
   })
 })
