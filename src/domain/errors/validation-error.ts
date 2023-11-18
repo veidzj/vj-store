61ec33716363885ab916277091ae35282e3ff442
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-11-18 13:07:12 -0300

    refactor: ensure ValidationError always have the first letter capitalized

--- src/domain/errors/validation-error.ts
+++ src/domain/errors/validation-error.ts
@@ -1,6 +1,10 @@
 export class ValidationError extends Error {
   constructor(errorMessage: string) {
-    super(errorMessage)
+    super(ValidationError.capitalizeFirstLetter(errorMessage))
     this.name = this.constructor.name
+  }
+
+  private static capitalizeFirstLetter(value: string): string {
+    return value.charAt(0).toUpperCase() + value.slice(1)
   }
 }
