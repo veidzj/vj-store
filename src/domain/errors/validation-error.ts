c1f393c28b82ea023d87490afd6463510b68c5ed
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-11-18 13:57:30 -0300

    feat: ensure ValidationError divide camel case words

--- src/domain/errors/validation-error.ts
+++ src/domain/errors/validation-error.ts
@@ -1,7 +1,13 @@
 export class ValidationError extends Error {
   constructor(errorMessage: string) {
-    super(ValidationError.capitalizeFirstLetter(errorMessage))
+    super(ValidationError.formatErrorMessage(errorMessage))
     this.name = this.constructor.name
+  }
+
+  private static formatErrorMessage(string: string): string {
+    const words = string.replace(/([A-Z])/g, ' $1').trim().toLowerCase().split(' ')
+    words[0] = ValidationError.capitalizeFirstLetter(words[0])
+    return words.join(' ')
   }
 
   private static capitalizeFirstLetter(value: string): string {
