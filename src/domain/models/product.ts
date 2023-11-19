36b43bbe1b64d3888d000e581036113a5f20e509
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-11-18 22:56:11 -0300

    feat: add Product model

---
+++ src/domain/models/product.ts
@@ -0,0 +1,11 @@
+export interface Product {
+  id: string
+  name: string
+  description: string
+  price: number
+  discount: number
+  category: string
+  imageUrls: string[]
+  quantity: number
+  slug: string
+}
