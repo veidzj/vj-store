0c8cea73a2bc78d3d104ae66131583e86ec6acb3
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-11-19 00:21:59 -0300

    feat: ensure AddProductController calls AddProduct with correct values

---
+++ src/domain/usecases/product/add-product.ts
@@ -0,0 +1,15 @@
+export interface AddProduct {
+  add: (input: AddProduct.Input) => Promise<void>
+}
+
+export namespace AddProduct {
+  export interface Input {
+    name: string
+    description: string
+    price: number
+    discountPercentage: number
+    category: string
+    imageUrls: string[]
+    quantity: number
+  }
+}
--- src/presentation/controllers/product/add-product-controller.ts
+++ src/presentation/controllers/product/add-product-controller.ts
@@ -1,14 +1,21 @@
-import { HttpHelper } from '@/presentation/helpers'
 import { type Controller, type Validation, type HttpResponse } from '@/presentation/protocols'
+import { HttpHelper } from '@/presentation/helpers'
+import { type AddProduct } from '@/domain/usecases/product/add-product'
 
 export class AddProductController implements Controller {
   private readonly httpHelper = new HttpHelper()
 
-  constructor(private readonly validation: Validation) {}
+  constructor(
+    private readonly validation: Validation,
+    private readonly addProduct: AddProduct
+  ) {}
 
   public handle = async(request: any): Promise<HttpResponse> => {
     try {
       this.validation.validate(request)
+
+      const { name, description, price, discountPercentage, category, imageUrls, quantity } = request
+      await this.addProduct.add({ name, description, price, discountPercentage, category, imageUrls, quantity })
       return {
         statusCode: 500,
         body: ''
