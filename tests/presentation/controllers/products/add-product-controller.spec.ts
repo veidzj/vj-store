9b68c1e31f2ba89bcb2f02b7b2628ff5f95129cb
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-11-18 23:07:52 -0300

    test: ensure AddProductController calls Validation with correct values

---
+++ tests/presentation/controllers/products/add-product-controller.spec.ts
@@ -0,0 +1,23 @@
+import { faker } from '@faker-js/faker'
+import { AddProductController } from '@/presentation/controllers/products/add-product-controller'
+import { ValidationSpy } from '@/tests/presentation/mocks'
+
+const mockRequest = (): AddProductController.Request => ({
+  name: faker.word.words(),
+  description: faker.word.words(),
+  price: faker.number.int(1000),
+  discountPercentage: faker.number.int({ min: 0, max: 100 }),
+  category: faker.word.words(),
+  imageUrls: [faker.internet.url(), faker.internet.url()],
+  quantity: faker.number.int(100)
+})
+
+describe('AddProductController', () => {
+  test('Should call Validation with correct values', async() => {
+    const validationSpy = new ValidationSpy()
+    const sut = new AddProductController(validationSpy)
+    const request = mockRequest()
+    await sut.handle(request)
+    expect(validationSpy.input).toEqual(request)
+  })
+})
