d7cb2461013e5665df1a237cf3f33c2e52ece424
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-11-20 18:29:39 -0300

    test: ensure DynamicProductMongoRepository throws if mongo throws

---
+++ tests/infra/db/mongodb/dynamic/product/dynamic-product-mongo-repository.spec.ts
@@ -0,0 +1,47 @@
+import { Collection } from 'mongodb'
+import { faker } from '@faker-js/faker'
+import { throwError } from '@/tests/domain/mocks'
+import { MongoHelper } from '@/infra/db/mongodb'
+import { DynamicProductMongoRepository } from '@/infra/db/mongodb/dynamic/product'
+import { type AddProductRepository } from '@/application/protocols/db/dynamic/product'
+
+let productCollection: Collection
+
+const makeSut = (): DynamicProductMongoRepository => {
+  return new DynamicProductMongoRepository()
+}
+
+const mockAddProductRepositoryInput = (): AddProductRepository.Input => ({
+  name: faker.word.words(),
+  description: faker.word.words(),
+  price: faker.number.int(1000),
+  discountPercentage: faker.number.int({ min: 0, max: 100 }),
+  category: faker.word.words(),
+  imageUrls: [faker.internet.url(), faker.internet.url()],
+  quantity: faker.number.int(100),
+  slug: faker.word.words()
+})
+
+describe('DynamicProductMongoRepository', () => {
+  beforeAll(async() => {
+    await MongoHelper.connect(process.env.MONGO_URL as string)
+  })
+
+  afterAll(async() => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async() => {
+    productCollection = MongoHelper.getCollection('products')
+    await productCollection.deleteMany({})
+  })
+
+  describe('add', () => {
+    test('Should throw if mongo throws', async() => {
+      const sut = makeSut()
+      jest.spyOn(Collection.prototype, 'insertOne').mockImplementationOnce(throwError)
+      const promise = sut.add(mockAddProductRepositoryInput())
+      await expect(promise).rejects.toThrow()
+    })
+  })
+})
