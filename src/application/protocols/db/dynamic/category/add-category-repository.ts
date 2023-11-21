3890122694f7714b05c08f761f1aeb0862900704
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-11-21 12:00:58 -0300

    feat: ensure DbAddCategory calls AddCategoryRepository with correct values

---
+++ src/application/protocols/db/dynamic/category/add-category-repository.ts
@@ -0,0 +1,9 @@
+export interface AddCategoryRepository {
+  add: (input: AddCategoryRepository.Input) => Promise<void>
+}
+
+export namespace AddCategoryRepository {
+  export interface Input {
+    name: string
+  }
+}
