74673d77f3ae9ebab2549810971c454e99f8a642
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-11-21 13:18:32 -0300

    test: ensure DynamicCategoryMongoRepository.add throws if mongo throws

---
+++ tests/infra/db/mongodb/dynamic/category/dynamic-category-mongo-repository.spec.ts
@@ -0,0 +1,40 @@
+import { Collection } from 'mongodb'
+import { faker } from '@faker-js/faker'
+import { throwError } from '@/tests/domain/mocks'
+import { MongoHelper } from '@/infra/db/mongodb'
+import { type AddCategoryRepository } from '@/application/protocols/db/dynamic/category'
+import { DynamicCategoryMongoRepository } from '@/infra/db/mongodb/dynamic/category'
+
+let categoryCollection: Collection
+
+const makeSut = (): DynamicCategoryMongoRepository => {
+  return new DynamicCategoryMongoRepository()
+}
+
+const mockAddCategoryRepositoryInput = (): AddCategoryRepository.Input => ({
+  name: faker.word.words()
+})
+
+describe('DynamicCategoryMongoRepository', () => {
+  beforeAll(async() => {
+    await MongoHelper.connect(process.env.MONGO_URL as string)
+  })
+
+  afterAll(async() => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async() => {
+    categoryCollection = MongoHelper.getCollection('categories')
+    await categoryCollection.deleteMany({})
+  })
+
+  describe('add', () => {
+    test('Should throw if mongo throws', async() => {
+      const sut = makeSut()
+      jest.spyOn(Collection.prototype, 'insertOne').mockImplementationOnce(throwError)
+      const promise = sut.add(mockAddCategoryRepositoryInput())
+      await expect(promise).rejects.toThrow()
+    })
+  })
+})
