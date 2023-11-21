2fe0d48912ac9073532e233e70bbe06d9d004435
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-11-21 17:38:31 -0300

    test: ensure AddCategoryController returns bad request if Validation throws an error

--- tests/presentation/controllers/category/add-category-controller.spec.ts
+++ tests/presentation/controllers/category/add-category-controller.spec.ts
@@ -1,6 +1,8 @@
 import { faker } from '@faker-js/faker'
 import { ValidationSpy } from '@/tests/presentation/mocks'
 import { AddCategoryController } from '@/presentation/controllers/category'
+import { ValidationError } from '@/domain/errors'
+import { HttpHelper } from '@/presentation/helpers'
 
 interface Sut {
   sut: AddCategoryController
@@ -21,12 +23,25 @@ const mockRequest = (): AddCategoryController.Request => ({
 })
 
 describe('AddCategoryController', () => {
+  const httpHelper = new HttpHelper()
+
   describe('Validation', () => {
     test('Should call Validation with correct values', async() => {
       const { sut, validationSpy } = makeSut()
       const request = mockRequest()
       await sut.handle(request)
       expect(validationSpy.input).toEqual(request)
+    })
+
+    test('Should return Bad Request if Validation throws an error', async() => {
+      const { sut, validationSpy } = makeSut()
+      const errorMessage = faker.word.words()
+      validationSpy.validate = jest.fn(() => {
+        throw new ValidationError(errorMessage)
+      })
+      const request = mockRequest()
+      const httpResponse = await sut.handle(request)
+      expect(httpResponse).toEqual(httpHelper.badRequest(new ValidationError(errorMessage)))
     })
   })
 })
