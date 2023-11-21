b4d5441ccd651749794c3daf529389c5b9a2c9d0
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-11-21 00:14:22 -0300

    test: ensure PositiveNumberValidation throws InvalidParamError if value is not a number

--- tests/validation/validators/positive-number-validation.spec.ts
+++ tests/validation/validators/positive-number-validation.spec.ts
@@ -9,10 +9,18 @@ const makeSut = (): PositiveNumberValidation => {
 }
 
 describe('PositiveNumberValidation', () => {
-  test('Should throw InvalidParamError if validation fails', () => {
+  test('Should throw InvalidParamError if value is less than 0', () => {
     const sut = makeSut()
     const error = (): void => {
       sut.validate({ [field]: -1 })
+    }
+    expect(error).toThrow(new InvalidParamError(field))
+  })
+
+  test('Should throw InvalidParamError if value is not a number', () => {
+    const sut = makeSut()
+    const error = (): void => {
+      sut.validate({ [field]: 'not_number' })
     }
     expect(error).toThrow(new InvalidParamError(field))
   })
