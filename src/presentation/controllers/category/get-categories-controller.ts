1ef6a7f5de435c043a18807a8c62ec8fc1db4e9c
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-11-23 13:50:48 -0300

    feat: ensure GetCategoriesController returns ok on success

--- src/presentation/controllers/category/get-categories-controller.ts
+++ src/presentation/controllers/category/get-categories-controller.ts
@@ -1,14 +1,14 @@
 import { type Controller, type HttpResponse } from '@/presentation/protocols'
 import { type GetCategories } from '@/domain/usecases/category'
+import { HttpHelper } from '@/presentation/helpers'
 
 export class GetCategoriesController implements Controller {
+  private readonly httpHelper = new HttpHelper()
+
   constructor(private readonly getCategories: GetCategories) {}
 
   public handle = async(request: any): Promise<HttpResponse> => {
-    await this.getCategories.get()
-    return {
-      statusCode: 500,
-      body: ''
-    }
+    const categories = await this.getCategories.get()
+    return this.httpHelper.ok(categories)
   }
 }
