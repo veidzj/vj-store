218efa3d4f0dd137dbff3d2d67239969bc3b613b
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-11-23 13:51:11 -0300

    test: ensure GetCategoriesController returns ok on success

--- tests/presentation/controllers/category/get-categories-controller.spec.ts
+++ tests/presentation/controllers/category/get-categories-controller.spec.ts
@@ -1,5 +1,6 @@
 import { GetCategoriesSpy } from '@/tests/presentation/mocks'
 import { GetCategoriesController } from '@/presentation/controllers/category'
+import { HttpHelper } from '@/presentation/helpers'
 
 interface Sut {
   sut: GetCategoriesController
@@ -16,10 +17,18 @@ const makeSut = (): Sut => {
 }
 
 describe('GetCategoriesController', () => {
+  const httpHelper = new HttpHelper()
+
   test('Should call GetCategories', async() => {
     const { sut, getCategoriesSpy } = makeSut()
     jest.spyOn(getCategoriesSpy, 'get')
     await sut.handle({})
     expect(getCategoriesSpy.get).toHaveBeenCalledTimes(1)
+  })
+
+  test('Should return OK on success', async() => {
+    const { sut, getCategoriesSpy } = makeSut()
+    const httpResponse = await sut.handle({})
+    expect(httpResponse).toEqual(httpHelper.ok(getCategoriesSpy.categories))
   })
 })
