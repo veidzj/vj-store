c467084cfeeb0a4f959c91c9998590c3e7ed3ce3
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-11-24 14:57:13 -0300

    test: ensure DbAddProduct calls CheckCategoryByNameRepository with correct value

--- tests/application/mocks/mock-db-category.ts
+++ tests/application/mocks/mock-db-category.ts
@@ -1,7 +1,17 @@
 import { type AddCategoryRepository } from '@/application/protocols/db/dynamic/category'
-import { type GetAllCategoriesRepository } from '@/application/protocols/db/static/category'
+import { type CheckCategoryByNameRepository, type GetAllCategoriesRepository } from '@/application/protocols/db/static/category'
 import { type Category } from '@/domain/models'
 import { mockCategories } from '@/tests/domain/mocks'
+
+export class CheckCategoryByNameRepositorySpy implements CheckCategoryByNameRepository {
+  public name: string
+  public output: boolean = false
+
+  public checkByName = async(name: string): Promise<boolean> => {
+    this.name = name
+    return this.output
+  }
+}
 
 export class AddCategoryRepositorySpy implements AddCategoryRepository {
   public input: AddCategoryRepository.Input
--- tests/application/usecases/product/db-add-product.spec.ts
+++ tests/application/usecases/product/db-add-product.spec.ts
@@ -1,44 +1,58 @@
-import { AddProductRepositorySpy } from '@/tests/application/mocks'
+import { AddProductRepositorySpy, CheckCategoryByNameRepositorySpy } from '@/tests/application/mocks'
 import { mockAddProductInput, throwError } from '@/tests/domain/mocks'
 import { DbAddProduct } from '@/application/usecases/product'
 import { ProductHelper } from '@/application/helpers'
 
 interface Sut {
   sut: DbAddProduct
+  checkCategoryByNameRepositorySpy: CheckCategoryByNameRepositorySpy
   addProductRepositorySpy: AddProductRepositorySpy
 }
 
 const makeSut = (): Sut => {
+  const checkCategoryByNameRepositorySpy = new CheckCategoryByNameRepositorySpy()
   const addProductRepositorySpy = new AddProductRepositorySpy()
-  const sut = new DbAddProduct(addProductRepositorySpy)
+  const sut = new DbAddProduct(checkCategoryByNameRepositorySpy, addProductRepositorySpy)
   return {
     sut,
+    checkCategoryByNameRepositorySpy,
     addProductRepositorySpy
   }
 }
 
 describe('DbAddProduct', () => {
-  test('Should call AddProductRepository with correct values', async() => {
-    const { sut, addProductRepositorySpy } = makeSut()
-    const addProductInput = mockAddProductInput()
-    await sut.add(addProductInput)
-    const expectedSlug = ProductHelper.generateSlug(addProductInput.name)
-    expect(addProductRepositorySpy.input).toEqual({
-      ...addProductInput,
-      slug: expectedSlug
+  describe('CheckCategoryByNameRepository', () => {
+    test('Should call CheckCategoryByNameRepository with correct value', async() => {
+      const { sut, checkCategoryByNameRepositorySpy } = makeSut()
+      const addProductInput = mockAddProductInput()
+      await sut.add(addProductInput)
+      expect(checkCategoryByNameRepositorySpy.name).toEqual(addProductInput.category)
     })
   })
 
-  test('Should throw if AddProductRepository throws', async() => {
-    const { sut, addProductRepositorySpy } = makeSut()
-    jest.spyOn(addProductRepositorySpy, 'add').mockImplementationOnce(throwError)
-    const promise = sut.add(mockAddProductInput())
-    await expect(promise).rejects.toThrow()
-  })
+  describe('AddProductRepository', () => {
+    test('Should call AddProductRepository with correct values', async() => {
+      const { sut, addProductRepositorySpy } = makeSut()
+      const addProductInput = mockAddProductInput()
+      await sut.add(addProductInput)
+      const expectedSlug = ProductHelper.generateSlug(addProductInput.name)
+      expect(addProductRepositorySpy.input).toEqual({
+        ...addProductInput,
+        slug: expectedSlug
+      })
+    })
 
-  test('Should not throw on success', async() => {
-    const { sut } = makeSut()
-    const promise = sut.add(mockAddProductInput())
-    await expect(promise).resolves.not.toThrow()
+    test('Should throw if AddProductRepository throws', async() => {
+      const { sut, addProductRepositorySpy } = makeSut()
+      jest.spyOn(addProductRepositorySpy, 'add').mockImplementationOnce(throwError)
+      const promise = sut.add(mockAddProductInput())
+      await expect(promise).rejects.toThrow()
+    })
+
+    test('Should not throw on success', async() => {
+      const { sut } = makeSut()
+      const promise = sut.add(mockAddProductInput())
+      await expect(promise).resolves.not.toThrow()
+    })
   })
 })
