0e19563667042b41edc7d1565939ab04d4b067f5
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-11-24 15:24:56 -0300

    feat: add StaticCategoryMongoRepository to AddProduct factory

--- src/main/factories/usecases/product/add-product-factory.ts
+++ src/main/factories/usecases/product/add-product-factory.ts
@@ -1,8 +1,10 @@
 import { type AddProduct } from '@/domain/usecases/product'
 import { DbAddProduct } from '@/application/usecases/product'
 import { DynamicProductMongoRepository } from '@/infra/db/mongodb/dynamic/product'
+import { StaticCategoryMongoRepository } from '@/infra/db/mongodb/static/category'
 
 export const makeDbAddProduct = (): AddProduct => {
   const dynamicProductMongoRepository = new DynamicProductMongoRepository()
-  return new DbAddProduct(dynamicProductMongoRepository)
+  const staticCategoryMongoRepository = new StaticCategoryMongoRepository()
+  return new DbAddProduct(staticCategoryMongoRepository, dynamicProductMongoRepository)
 }
