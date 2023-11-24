cf383cbab20086ef690f183cfe0c281e71456c08
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-11-24 14:56:45 -0300

    feat: ensure DbAddProduct calls CheckCategoryByNameRepository with correct value

--- src/application/usecases/product/db-add-product.ts
+++ src/application/usecases/product/db-add-product.ts
@@ -1,11 +1,16 @@
 import { ProductHelper } from '@/application/helpers/product-helper'
+import { type CheckCategoryByNameRepository } from '@/application/protocols/db/static/category'
 import { type AddProductRepository } from '@/application/protocols/db/dynamic/product'
 import { type AddProduct } from '@/domain/usecases/product'
 
 export class DbAddProduct implements AddProduct {
-  constructor(private readonly addProductRepository: AddProductRepository) {}
+  constructor(
+    private readonly checkCategoryByNameRepository: CheckCategoryByNameRepository,
+    private readonly addProductRepository: AddProductRepository
+  ) {}
 
   public add = async(input: AddProduct.Input): Promise<void> => {
+    await this.checkCategoryByNameRepository.checkByName(input.category)
     const slug = ProductHelper.generateSlug(input.name)
     const product = { ...input, slug }
     await this.addProductRepository.add(product)
