40949f207a6b66c4bbc1a8acd9ae4007ee227450
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-11-24 15:19:57 -0300

    feat: ensure StaticCategoryMongoRepository.checkByName throws if mongo throws

--- src/infra/db/mongodb/static/category/static-category-mongo-repository.ts
+++ src/infra/db/mongodb/static/category/static-category-mongo-repository.ts
@@ -1,7 +1,13 @@
 import { MongoHelper } from '@/infra/db/mongodb/mongo-helper'
-import { type GetAllCategoriesRepository } from '@/application/protocols/db/static/category'
+import { type CheckCategoryByNameRepository, type GetAllCategoriesRepository } from '@/application/protocols/db/static/category'
 
-export class StaticCategoryMongoRepository implements GetAllCategoriesRepository {
+export class StaticCategoryMongoRepository implements CheckCategoryByNameRepository, GetAllCategoriesRepository {
+  public checkByName = async(name: string): Promise<boolean> => {
+    const categoryCollection = MongoHelper.getCollection('categories')
+    const category = await categoryCollection.findOne({ $where: name })
+    return category !== null
+  }
+
   public getAll = async(): Promise<GetAllCategoriesRepository.Output> => {
     const categoryCollection = MongoHelper.getCollection('categories')
     const categories = await categoryCollection.find().toArray()
