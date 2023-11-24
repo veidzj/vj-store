35b1df99a944304f5658bca9cc7dc0be0133a6b9
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-11-24 17:48:47 -0300

    feat: ensure DynamicProductMongoRepository.update throws if mongo throws

--- src/infra/db/mongodb/dynamic/product/dynamic-product-mongo-repository.ts
+++ src/infra/db/mongodb/dynamic/product/dynamic-product-mongo-repository.ts
@@ -1,9 +1,18 @@
 import { MongoHelper } from '@/infra/db/mongodb/mongo-helper'
-import { type AddProductRepository } from '@/application/protocols/db/dynamic/product'
+import { type AddProductRepository, type UpdateProductRepository } from '@/application/protocols/db/dynamic/product'
+import { ObjectId } from 'mongodb'
 
-export class DynamicProductMongoRepository implements AddProductRepository {
+export class DynamicProductMongoRepository implements AddProductRepository, UpdateProductRepository {
   public add = async(input: AddProductRepository.Input): Promise<void> => {
     const productCollection = MongoHelper.getCollection('products')
     await productCollection.insertOne(input)
+  }
+
+  public update = async(input: UpdateProductRepository.Input): Promise<void> => {
+    const productCollection = MongoHelper.getCollection('products')
+    await productCollection.updateOne(
+      { _id: new ObjectId(input.productId) },
+      { $set: input }
+    )
   }
 }
