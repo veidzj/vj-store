3ebcf74135587ec50ff51eb6551112ce150eae9a
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-11-25 16:22:39 -0300

    test: ensure POST category returns 200 if valid accessToken is provided

--- tests/main/routes/category-route.test.ts
+++ tests/main/routes/category-route.test.ts
@@ -1,6 +1,7 @@
-import { type Collection } from 'mongodb'
+import { sign } from 'jsonwebtoken'
 import request from 'supertest'
 import { faker } from '@faker-js/faker'
+import { type Collection } from 'mongodb'
 import { type Express } from 'express'
 import { setupApp } from '@/main/config/app'
 import { env } from '@/main/config'
@@ -14,6 +15,22 @@ const categoryData = {
 let categoryCollection: Collection
 let accountCollection: Collection
 let app: Express
+
+const mockAccessToken = async(): Promise<string> => {
+  const res = await accountCollection.insertOne({
+    username: 'valid_username',
+    email: 'valid_email@mail.com',
+    password: 'valid_password',
+    role: 'admin'
+  })
+  const id = res.insertedId.toHexString()
+  const accessToken = sign({ id }, env.jwtSecret)
+  await accountCollection.updateOne(
+    { _id: res.insertedId },
+    { $set: { accessToken } }
+  )
+  return accessToken
+}
 
 describe('Category Routes', () => {
   beforeAll(async() => {
@@ -38,6 +55,15 @@ describe('Category Routes', () => {
         .post(categoryRoute)
         .send(categoryData)
         .expect(401)
+    })
+
+    test('Should return 200 if valid accessToken is provided', async() => {
+      const accessToken = await mockAccessToken()
+      await request(app)
+        .post(categoryRoute)
+        .set('x-access-token', accessToken)
+        .send(categoryData)
+        .expect(200)
     })
   })
 })
