58532df8730b77ac1c1002e4250fb59b56104722
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-11-25 16:42:20 -0300

    test: ensure POST product returns 200 if valid accessToken is provided and category exists

--- tests/main/routes/product-route.test.ts
+++ tests/main/routes/product-route.test.ts
@@ -1,3 +1,4 @@
+import { sign } from 'jsonwebtoken'
 import request from 'supertest'
 import { type Collection } from 'mongodb'
 import { type Express } from 'express'
@@ -10,8 +11,25 @@ const productRoute: string = '/api/product'
 const productData = mockAddProductInput()
 
 let productCollection: Collection
+let categoryCollection: Collection
 let accountCollection: Collection
 let app: Express
+
+const mockAccessToken = async(): Promise<string> => {
+  const res = await accountCollection.insertOne({
+    username: 'valid_username',
+    email: 'valid_email@mail.com',
+    password: 'valid_password',
+    role: 'admin'
+  })
+  const id = res.insertedId.toHexString()
+  const accessToken = sign({ id }, env.jwtSecret)
+  await accountCollection.updateOne(
+    { _id: res.insertedId },
+    { $set: { accessToken } }
+  )
+  return accessToken
+}
 
 describe('Product Routes', () => {
   beforeAll(async() => {
@@ -25,8 +43,10 @@ describe('Product Routes', () => {
 
   beforeEach(async() => {
     productCollection = MongoHelper.getCollection('products')
+    categoryCollection = MongoHelper.getCollection('categories')
     accountCollection = MongoHelper.getCollection('accounts')
     await productCollection.deleteMany({})
+    await categoryCollection.deleteMany({})
     await accountCollection.deleteMany({})
   })
 
@@ -36,6 +56,16 @@ describe('Product Routes', () => {
         .post(productRoute)
         .send(productData)
         .expect(401)
+    })
+
+    test('Should return 200 if valid accessToken is provided and category exists', async() => {
+      await categoryCollection.insertOne({ name: productData.category })
+      const accessToken = await mockAccessToken()
+      await request(app)
+        .post(productRoute)
+        .set('x-access-token', accessToken)
+        .send(productData)
+        .expect(200)
     })
   })
 })
