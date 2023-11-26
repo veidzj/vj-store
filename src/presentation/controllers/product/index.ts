a4c2c978a26cfb7680f5115b36899e551fd6f444
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-11-26 16:33:46 -0300

    fix: add controller suffix to GetProductBySlugController

--- src/presentation/controllers/product/index.ts
+++ src/presentation/controllers/product/index.ts
@@ -1,4 +1,4 @@
 export * from './add-product-controller'
 export * from './get-all-products-controller'
-export * from './get-product-by-slug'
+export * from './get-product-by-slug-controller'
 export * from './update-product-controller'
