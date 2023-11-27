2f0d9fdd69d778e88bba6720be56b594948e0f08
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-11-27 17:20:33 -0300

    test: ensure GetProductsByCategoryController returns badRequest if GetProductsByCategory throws a CategoryError

--- tests/presentation/controllers/product/get-products-by-category-controller.spec.ts
+++ tests/presentation/controllers/product/get-products-by-category-controller.spec.ts
@@ -2,6 +2,8 @@ import { faker } from '@faker-js/faker'
 
 import { GetProductsByCategorySpy } from '@/tests/presentation/mocks'
 import { GetProductsByCategoryController } from '@/presentation/controllers/product'
+import { HttpHelper } from '@/presentation/helpers'
+import { CategoryError } from '@/domain/errors'
 
 interface Sut {
   sut: GetProductsByCategoryController
@@ -28,5 +30,14 @@ describe('GetProductsByCategoryController', () => {
     const request = mockRequest()
     await sut.handle(request)
     expect(getProductsByCategorySpy.getByCategory).toHaveBeenCalledWith(request.category)
+  })
+
+  test('Should return badRequest if GetProductsByCategory throws a CategoryError', async() => {
+    const { sut, getProductsByCategorySpy } = makeSut()
+    const errorMessage = faker.word.words()
+    jest.spyOn(getProductsByCategorySpy, 'getByCategory').mockImplementationOnce(() => { throw new CategoryError(errorMessage) })
+    const request = mockRequest()
+    const response = await sut.handle(request)
+    expect(response).toEqual(HttpHelper.badRequest(new CategoryError(errorMessage)))
   })
 })
