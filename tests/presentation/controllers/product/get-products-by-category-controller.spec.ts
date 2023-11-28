411647109166834c6f1eaec59a9cb437671f3aaa
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-11-28 17:18:35 -0300

    test: ensure GetProductsByCategoryController calls GetProductsByCategory with correct values with and without pagination params

--- tests/presentation/controllers/product/get-products-by-category-controller.spec.ts
+++ tests/presentation/controllers/product/get-products-by-category-controller.spec.ts
@@ -4,6 +4,7 @@ import { GetProductsByCategorySpy } from '@/tests/presentation/mocks'
 import { throwError } from '@/tests/domain/mocks'
 import { GetProductsByCategoryController } from '@/presentation/controllers/product'
 import { HttpHelper } from '@/presentation/helpers'
+import { DEFAULT_PAGE, DEFAULT_LIMIT } from '@/presentation/constants'
 import { CategoryError } from '@/domain/errors'
 
 interface Sut {
@@ -20,17 +21,29 @@ const makeSut = (): Sut => {
   }
 }
 
-const mockRequest = (): GetProductsByCategoryController.Request => ({
+const mockRequestWithoutPageAndLimit = (): GetProductsByCategoryController.Request => ({
+  category: faker.word.words()
+})
+
+const mockRequestWithPageAndLimit = (): GetProductsByCategoryController.Request => ({
   category: faker.word.words(),
   page: faker.number.int().toString(),
   limit: faker.number.int().toString()
 })
 
 describe('GetProductsByCategoryController', () => {
-  test('Should call GetProductsByCategory with correct values', async() => {
+  test('Should call GetProductsByCategory with correct values without page and limit', async() => {
     const { sut, getProductsByCategorySpy } = makeSut()
     jest.spyOn(getProductsByCategorySpy, 'getByCategory')
-    const request = mockRequest()
+    const request = mockRequestWithoutPageAndLimit()
+    await sut.handle(request)
+    expect(getProductsByCategorySpy.getByCategory).toHaveBeenCalledWith(request.category, DEFAULT_PAGE, DEFAULT_LIMIT)
+  })
+
+  test('Should call GetProductsByCategory with correct values with page and limit', async() => {
+    const { sut, getProductsByCategorySpy } = makeSut()
+    jest.spyOn(getProductsByCategorySpy, 'getByCategory')
+    const request = mockRequestWithPageAndLimit()
     await sut.handle(request)
     expect(getProductsByCategorySpy.getByCategory).toHaveBeenCalledWith(request.category, Number(request.page), Number(request.limit))
   })
@@ -39,20 +52,20 @@ describe('GetProductsByCategoryController', () => {
     const { sut, getProductsByCategorySpy } = makeSut()
     const errorMessage = faker.word.words()
     jest.spyOn(getProductsByCategorySpy, 'getByCategory').mockImplementationOnce(() => { throw new CategoryError(errorMessage) })
-    const response = await sut.handle(mockRequest())
+    const response = await sut.handle(mockRequestWithPageAndLimit())
     expect(response).toEqual(HttpHelper.badRequest(new CategoryError(errorMessage)))
   })
 
   test('Should return serverError if GetProductsByCategory throws', async() => {
     const { sut, getProductsByCategorySpy } = makeSut()
     jest.spyOn(getProductsByCategorySpy, 'getByCategory').mockImplementationOnce(throwError)
-    const response = await sut.handle(mockRequest())
+    const response = await sut.handle(mockRequestWithPageAndLimit())
     expect(response).toEqual(HttpHelper.serverError(new Error()))
   })
 
   test('Should return ok on success', async() => {
     const { sut, getProductsByCategorySpy } = makeSut()
-    const response = await sut.handle(mockRequest())
+    const response = await sut.handle(mockRequestWithPageAndLimit())
     expect(response).toEqual(HttpHelper.ok(getProductsByCategorySpy.products))
   })
 })
