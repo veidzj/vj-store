0fd6b0e5c1d3bb875bffd324464ff8fe0ac514b3
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-11-28 02:56:26 -0300

    refactor: move pagination constants to its own folder

--- src/presentation/controllers/product/get-all-products-controller.ts
+++ src/presentation/controllers/product/get-all-products-controller.ts
@@ -1,14 +1,15 @@
 import { type Controller, type Response } from '@/presentation/protocols'
 import { HttpHelper } from '@/presentation/helpers'
 import { type GetAllProducts } from '@/domain/usecases/product'
+import { DEFAULT_PAGE, DEFAULT_LIMIT } from '@/presentation/constants'
 
 export class GetAllProductsController implements Controller {
   constructor(private readonly getAllProducts: GetAllProducts) {}
 
   public handle = async(request: GetAllProductsController.Request): Promise<Response> => {
     try {
-      const page = Number(request.page) || 1
-      const limit = Number(request.limit) || 25
+      const page = Number(request.page) || DEFAULT_PAGE
+      const limit = Number(request.limit) || DEFAULT_LIMIT
       const products = await this.getAllProducts.getAll(page, limit)
       return HttpHelper.ok(products)
     } catch (error) {
--- src/presentation/controllers/product/get-products-by-category-controller.ts
+++ src/presentation/controllers/product/get-products-by-category-controller.ts
@@ -1,5 +1,6 @@
 import { type Controller, type Response } from '@/presentation/protocols'
 import { HttpHelper } from '@/presentation/helpers'
+import { DEFAULT_PAGE, DEFAULT_LIMIT } from '@/presentation/constants'
 import { type GetProductsByCategory } from '@/domain/usecases/product'
 import { CategoryError } from '@/domain/errors'
 
@@ -9,8 +10,8 @@ export class GetProductsByCategoryController implements Controller {
   public handle = async(request: GetProductsByCategoryController.Request): Promise<Response> => {
     try {
       const { category } = request
-      const page = Number(request.page) || 1
-      const limit = Number(request.limit) || 25
+      const page = Number(request.page) || DEFAULT_PAGE
+      const limit = Number(request.limit) || DEFAULT_LIMIT
       const products = await this.getProductsByCategory.getByCategory(category, page, limit)
       return HttpHelper.ok(products)
     } catch (error) {
