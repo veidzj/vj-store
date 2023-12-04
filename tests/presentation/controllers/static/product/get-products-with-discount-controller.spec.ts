2cde72397005cfda1eeddccaf137f4af61cf3792
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-12-04 15:26:24 -0300

    test: add pagination to GetProductsWithDiscountController

--- tests/presentation/controllers/static/product/get-products-with-discount-controller.spec.ts
+++ tests/presentation/controllers/static/product/get-products-with-discount-controller.spec.ts
@@ -1,7 +1,10 @@
+import { faker } from '@faker-js/faker'
+
 import { GetProductsWithDiscountSpy } from '@/tests/presentation/mocks'
 import { throwError } from '@/tests/domain/mocks'
 import { GetProductsWithDiscountController } from '@/presentation/controllers/static/product'
 import { HttpHelper } from '@/presentation/helpers'
+import { DEFAULT_PAGE, DEFAULT_LIMIT } from '@/presentation/constants'
 
 interface Sut {
   sut: GetProductsWithDiscountController
@@ -17,24 +20,39 @@ const makeSut = (): Sut => {
   }
 }
 
+const mockRequestWithoutPageAndLimit = (): GetProductsWithDiscountController.Request => ({})
+
+const mockRequestWithPageAndLimit = (): GetProductsWithDiscountController.Request => ({
+  page: faker.number.int().toString(),
+  limit: faker.number.int().toString()
+})
+
 describe('GetProductsWithDiscountController', () => {
-  test('Should call GetProductsWithDiscount', async() => {
+  test('Should call GetProductsWithDiscount with correct values without page and limit', async() => {
     const { sut, getProductsWithDiscountSpy } = makeSut()
     jest.spyOn(getProductsWithDiscountSpy, 'getWithDiscount')
-    await sut.handle()
-    expect(getProductsWithDiscountSpy.getWithDiscount).toHaveBeenCalledTimes(1)
+    await sut.handle(mockRequestWithoutPageAndLimit())
+    expect(getProductsWithDiscountSpy.getWithDiscount).toHaveBeenCalledWith(DEFAULT_PAGE, DEFAULT_LIMIT)
+  })
+
+  test('Should call GetProductsWithDiscount with correct values with page and limit', async() => {
+    const { sut, getProductsWithDiscountSpy } = makeSut()
+    jest.spyOn(getProductsWithDiscountSpy, 'getWithDiscount')
+    const request = mockRequestWithPageAndLimit()
+    await sut.handle(request)
+    expect(getProductsWithDiscountSpy.getWithDiscount).toHaveBeenCalledWith(Number(request.page), Number(request.limit))
   })
 
   test('Should return serverError if GetProductsWithDiscount throws', async() => {
     const { sut, getProductsWithDiscountSpy } = makeSut()
     jest.spyOn(getProductsWithDiscountSpy, 'getWithDiscount').mockImplementationOnce(throwError)
-    const response = await sut.handle()
+    const response = await sut.handle(mockRequestWithoutPageAndLimit())
     expect(response).toEqual(HttpHelper.serverError(new Error()))
   })
 
   test('Should return ok on success', async() => {
     const { sut, getProductsWithDiscountSpy } = makeSut()
-    const response = await sut.handle()
+    const response = await sut.handle(mockRequestWithoutPageAndLimit())
     expect(response).toEqual(HttpHelper.ok(getProductsWithDiscountSpy.products))
   })
 })
