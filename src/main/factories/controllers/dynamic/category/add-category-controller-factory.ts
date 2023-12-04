642ccbf14b3472d32a0e2e3e7a5a363c726c4538
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-12-04 14:15:35 -0300

    refactor: change AddCategoryFactory to a class

--- src/main/factories/controllers/dynamic/category/add-category-controller-factory.ts
+++ src/main/factories/controllers/dynamic/category/add-category-controller-factory.ts
@@ -1,12 +1,12 @@
 import { LogControllerDecoratorFactory } from '@/main/factories/decorators/log'
 import { makeAddCategoryValidation } from '@/main/factories/validations/category'
-import { makeDbAddCategory } from '@/main/factories/usecases/dynamic/category'
+import { AddCategoryFactory } from '@/main/factories/usecases/dynamic/category'
 import { type Controller } from '@/presentation/protocols'
 import { AddCategoryController } from '@/presentation/controllers/dynamic/category'
 
 export class AddCategoryControllerFactory {
   public static makeAddCategoryController = (): Controller => {
-    const controller = new AddCategoryController(makeAddCategoryValidation(), makeDbAddCategory())
+    const controller = new AddCategoryController(makeAddCategoryValidation(), AddCategoryFactory.makeAddCategory())
     return LogControllerDecoratorFactory.makeLogControllerDecorator(controller)
   }
 }
--- src/main/factories/usecases/dynamic/category/add-category-factory.ts
+++ src/main/factories/usecases/dynamic/category/add-category-factory.ts
@@ -2,7 +2,9 @@ import { type AddCategory } from '@/domain/usecases/dynamic/category'
 import { DbAddCategory } from '@/application/usecases/dynamic/category'
 import { DynamicCategoryMongoRepository } from '@/infra/db/mongodb/dynamic/category'
 
-export const makeDbAddCategory = (): AddCategory => {
-  const dynamicCategoryMongoRepository = new DynamicCategoryMongoRepository()
-  return new DbAddCategory(dynamicCategoryMongoRepository)
+export class AddCategoryFactory {
+  public static makeAddCategory = (): AddCategory => {
+    const dynamicCategoryMongoRepository = new DynamicCategoryMongoRepository()
+    return new DbAddCategory(dynamicCategoryMongoRepository)
+  }
 }
