8d8e8a5d47cb8aed19c2e2a479280de537c5b89c
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-12-04 14:16:53 -0300

    refactor: change UpdateProductFactory to a class

--- src/main/factories/controllers/dynamic/product/update-product-controller-factory.ts
+++ src/main/factories/controllers/dynamic/product/update-product-controller-factory.ts
@@ -1,12 +1,12 @@
 import { LogControllerDecoratorFactory } from '@/main/factories/decorators/log'
 import { makeAddProductValidation } from '@/main/factories/validations/product'
-import { makeDbUpdateProduct } from '@/main/factories/usecases/dynamic/product'
+import { UpdateProductFactory } from '@/main/factories/usecases/dynamic/product'
 import { type Controller } from '@/presentation/protocols'
 import { UpdateProductController } from '@/presentation/controllers/dynamic/product'
 
 export class UpdateProductControllerFactory {
   public static makeUpdateProductController = (): Controller => {
-    const controller = new UpdateProductController(makeAddProductValidation(), makeDbUpdateProduct())
+    const controller = new UpdateProductController(makeAddProductValidation(), UpdateProductFactory.makeUpdateProduct())
     return LogControllerDecoratorFactory.makeLogControllerDecorator(controller)
   }
 }
--- src/main/factories/usecases/dynamic/product/update-product-factory.ts
+++ src/main/factories/usecases/dynamic/product/update-product-factory.ts
@@ -3,8 +3,10 @@ import { DbUpdateProduct } from '@/application/usecases/dynamic/product'
 import { DynamicProductMongoRepository } from '@/infra/db/mongodb/dynamic/product'
 import { StaticProductMongoRepository } from '@/infra/db/mongodb/static/product'
 
-export const makeDbUpdateProduct = (): UpdateProduct => {
-  const dynamicProductMongoRepository = new DynamicProductMongoRepository()
-  const staticProductMongoRepository = new StaticProductMongoRepository()
-  return new DbUpdateProduct(staticProductMongoRepository, dynamicProductMongoRepository)
+export class UpdateProductFactory {
+  public static makeUpdateProduct = (): UpdateProduct => {
+    const dynamicProductMongoRepository = new DynamicProductMongoRepository()
+    const staticProductMongoRepository = new StaticProductMongoRepository()
+    return new DbUpdateProduct(staticProductMongoRepository, dynamicProductMongoRepository)
+  }
 }
