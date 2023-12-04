342fd732c11433d90d07e94782152546e7bfd7a0
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-12-04 14:16:24 -0300

    refactor: change AddProductFactory to a class

--- src/main/factories/controllers/dynamic/product/add-product-controller-factory.ts
+++ src/main/factories/controllers/dynamic/product/add-product-controller-factory.ts
@@ -1,12 +1,12 @@
 import { LogControllerDecoratorFactory } from '@/main/factories/decorators/log'
 import { makeAddProductValidation } from '@/main/factories/validations/product'
-import { makeDbAddProduct } from '@/main/factories/usecases/dynamic/product'
+import { AddProductFactory } from '@/main/factories/usecases/dynamic/product'
 import { type Controller } from '@/presentation/protocols'
 import { AddProductController } from '@/presentation/controllers/dynamic/product'
 
 export class AddProductControllerFactory {
   public static makeAddProductController = (): Controller => {
-    const controller = new AddProductController(makeAddProductValidation(), makeDbAddProduct())
+    const controller = new AddProductController(makeAddProductValidation(), AddProductFactory.makeAddProduct())
     return LogControllerDecoratorFactory.makeLogControllerDecorator(controller)
   }
 }
--- src/main/factories/usecases/dynamic/product/add-product-factory.ts
+++ src/main/factories/usecases/dynamic/product/add-product-factory.ts
@@ -3,8 +3,10 @@ import { DbAddProduct } from '@/application/usecases/dynamic/product'
 import { DynamicProductMongoRepository } from '@/infra/db/mongodb/dynamic/product'
 import { StaticCategoryMongoRepository } from '@/infra/db/mongodb/static/category'
 
-export const makeDbAddProduct = (): AddProduct => {
-  const dynamicProductMongoRepository = new DynamicProductMongoRepository()
-  const staticCategoryMongoRepository = new StaticCategoryMongoRepository()
-  return new DbAddProduct(staticCategoryMongoRepository, dynamicProductMongoRepository)
+export class AddProductFactory {
+  public static makeAddProduct = (): AddProduct => {
+    const dynamicProductMongoRepository = new DynamicProductMongoRepository()
+    const staticCategoryMongoRepository = new StaticCategoryMongoRepository()
+    return new DbAddProduct(staticCategoryMongoRepository, dynamicProductMongoRepository)
+  }
 }
