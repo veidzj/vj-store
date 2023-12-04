a96d7f503c694d7d66029cf593bfec99b794fa7c
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-12-04 02:45:38 -0300

    refactor: change GetProductBySlugControllerFactory to a class

--- src/main/factories/controllers/static/product/get-product-by-slug-controller-factory.ts
+++ src/main/factories/controllers/static/product/get-product-by-slug-controller-factory.ts
@@ -3,7 +3,9 @@ import { makeDbGetProductBySlug } from '@/main/factories/usecases/static/product
 import { type Controller } from '@/presentation/protocols'
 import { GetProductBySlugController } from '@/presentation/controllers/static/product'
 
-export const makeGetProductBySlugController = (): Controller => {
-  const controller = new GetProductBySlugController(makeDbGetProductBySlug())
-  return makeLogControllerDecorator(controller)
+export class GetProductBySlugControllerFactory {
+  public static makeGetProductBySlugController = (): Controller => {
+    const controller = new GetProductBySlugController(makeDbGetProductBySlug())
+    return makeLogControllerDecorator(controller)
+  }
 }
