01a2fdc581d1e444225a1dc7f97518de89330dbf
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-12-04 02:42:54 -0300

    refactor: change UpdateProductControllerFactory to a class

--- src/main/factories/controllers/dynamic/product/update-product-controller-factory.ts
+++ src/main/factories/controllers/dynamic/product/update-product-controller-factory.ts
@@ -4,7 +4,9 @@ import { makeDbUpdateProduct } from '@/main/factories/usecases/dynamic/product'
 import { type Controller } from '@/presentation/protocols'
 import { UpdateProductController } from '@/presentation/controllers/dynamic/product'
 
-export const makeUpdateProductController = (): Controller => {
-  const controller = new UpdateProductController(makeAddProductValidation(), makeDbUpdateProduct())
-  return makeLogControllerDecorator(controller)
+export class UpdateProductControllerFactory {
+  public static makeUpdateProductController = (): Controller => {
+    const controller = new UpdateProductController(makeAddProductValidation(), makeDbUpdateProduct())
+    return makeLogControllerDecorator(controller)
+  }
 }
--- src/main/routes/product-route.ts
+++ src/main/routes/product-route.ts
@@ -1,13 +1,13 @@
 import { type Router } from 'express'
 
 import { ExpressRouteAdapter } from '@/main/adapters'
-import { AddProductControllerFactory, makeUpdateProductController } from '@/main/factories/controllers/dynamic/product'
+import { AddProductControllerFactory, UpdateProductControllerFactory } from '@/main/factories/controllers/dynamic/product'
 import { makeGetProductBySlugController, makeGetProductsByCategoryController } from '@/main/factories/controllers/static/product'
 import { adminAuth } from '@/main/middlewares/auth'
 
 export default (router: Router): void => {
   router.post('/product', adminAuth, ExpressRouteAdapter.adapt(AddProductControllerFactory.makeAddProductController()))
-  router.put('/product/:id', adminAuth, ExpressRouteAdapter.adapt(makeUpdateProductController()))
+  router.put('/product/:id', adminAuth, ExpressRouteAdapter.adapt(UpdateProductControllerFactory.makeUpdateProductController()))
   router.get('/product/slug/:slug', ExpressRouteAdapter.adapt(makeGetProductBySlugController()))
   router.get('/product/category/:category', ExpressRouteAdapter.adapt(makeGetProductsByCategoryController()))
 }
