f93e78a84b3238ba8f57944d0172fb94b9a216d9
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-12-04 14:22:38 -0300

    refactor: change GetAllCategoriesFactory to a class

--- src/main/factories/controllers/static/category/get-all-categories-controller-factory.ts
+++ src/main/factories/controllers/static/category/get-all-categories-controller-factory.ts
@@ -1,11 +1,11 @@
 import { LogControllerDecoratorFactory } from '@/main/factories/decorators/log'
-import { makeDbGetAllCategories } from '@/main/factories/usecases/static/category'
+import { GetAllCategoriesFactory } from '@/main/factories/usecases/static/category'
 import { type Controller } from '@/presentation/protocols'
 import { GetAllCategoriesController } from '@/presentation/controllers/static/category'
 
 export class GetAllCategoriesControllerFactory {
   public static makeGetAllCategoriesController = (): Controller => {
-    const controller = new GetAllCategoriesController(makeDbGetAllCategories())
+    const controller = new GetAllCategoriesController(GetAllCategoriesFactory.makeGetAllCategories())
     return LogControllerDecoratorFactory.makeLogControllerDecorator(controller)
   }
 }
--- src/main/factories/usecases/static/category/get-categories-factory.ts
+++ src/main/factories/usecases/static/category/get-categories-factory.ts
@@ -2,7 +2,9 @@ import { type GetAllCategories } from '@/domain/usecases/static/category'
 import { DbGetAllCategories } from '@/application/usecases/static/category'
 import { StaticCategoryMongoRepository } from '@/infra/db/mongodb/static/category'
 
-export const makeDbGetAllCategories = (): GetAllCategories => {
-  const staticCategoryMongoRepository = new StaticCategoryMongoRepository()
-  return new DbGetAllCategories(staticCategoryMongoRepository)
+export class GetAllCategoriesFactory {
+  public static makeGetAllCategories = (): GetAllCategories => {
+    const staticCategoryMongoRepository = new StaticCategoryMongoRepository()
+    return new DbGetAllCategories(staticCategoryMongoRepository)
+  }
 }
