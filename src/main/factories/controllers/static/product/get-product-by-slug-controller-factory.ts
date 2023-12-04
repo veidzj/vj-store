1170c56c385277c6825e56b43e5224bfcedc72b7
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-12-04 14:23:11 -0300

    refactor: change GetProductBySlugFactory to a class

--- src/main/factories/controllers/static/product/get-product-by-slug-controller-factory.ts
+++ src/main/factories/controllers/static/product/get-product-by-slug-controller-factory.ts
@@ -1,11 +1,11 @@
 import { LogControllerDecoratorFactory } from '@/main/factories/decorators/log'
-import { makeDbGetProductBySlug } from '@/main/factories/usecases/static/product'
+import { GetProductBySlugFactory } from '@/main/factories/usecases/static/product'
 import { type Controller } from '@/presentation/protocols'
 import { GetProductBySlugController } from '@/presentation/controllers/static/product'
 
 export class GetProductBySlugControllerFactory {
   public static makeGetProductBySlugController = (): Controller => {
-    const controller = new GetProductBySlugController(makeDbGetProductBySlug())
+    const controller = new GetProductBySlugController(GetProductBySlugFactory.makeGetProductBySlug())
     return LogControllerDecoratorFactory.makeLogControllerDecorator(controller)
   }
 }
--- src/main/factories/usecases/static/product/get-product-by-slug-factory.ts
+++ src/main/factories/usecases/static/product/get-product-by-slug-factory.ts
@@ -2,7 +2,9 @@ import { type GetProductBySlug } from '@/domain/usecases/static/product'
 import { DbGetProductBySlug } from '@/application/usecases/static/product'
 import { StaticProductMongoRepository } from '@/infra/db/mongodb/static/product'
 
-export const makeDbGetProductBySlug = (): GetProductBySlug => {
-  const staticProductMongoRepository = new StaticProductMongoRepository()
-  return new DbGetProductBySlug(staticProductMongoRepository)
+export class GetProductBySlugFactory {
+  public static makeGetProductBySlug = (): GetProductBySlug => {
+    const staticProductMongoRepository = new StaticProductMongoRepository()
+    return new DbGetProductBySlug(staticProductMongoRepository)
+  }
 }
