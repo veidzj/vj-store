3c06071413d5598b7314274bb6b9bf404afe85ab
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-12-04 16:04:06 -0300

    test: ensure StaticProductMongoRepository.getWithDiscount returns an empty list if there are no products

--- tests/infra/db/mongodb/static/product/static-product-mongo-repository.spec.ts
+++ tests/infra/db/mongodb/static/product/static-product-mongo-repository.spec.ts
@@ -132,5 +132,11 @@ describe('StaticProductMongoRepository', () => {
       const promise = sut.getWithDiscount()
       await expect(promise).rejects.toThrow()
     })
+
+    test('Should return an empty list if there are no products', async() => {
+      const sut = makeSut()
+      const products = await sut.getWithDiscount()
+      expect(products.length).toBe(0)
+    })
   })
 })
