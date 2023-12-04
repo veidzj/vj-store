3484501435246f2a27cbe876da39af4e14202c3b
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-12-04 15:17:16 -0300

    feat: add pagination to GetProductsWithDiscountController

--- src/presentation/controllers/static/product/get-products-with-discount.ts
+++ src/presentation/controllers/static/product/get-products-with-discount.ts
@@ -1,16 +1,26 @@
 import { type Controller, type Response } from '@/presentation/protocols'
 import { HttpHelper } from '@/presentation/helpers'
+import { DEFAULT_PAGE, DEFAULT_LIMIT } from '@/presentation/constants'
 import { type GetProductsWithDiscount } from '@/domain/usecases/static/product'
 
 export class GetProductsWithDiscountController implements Controller {
   constructor(private readonly getProductsWithDiscount: GetProductsWithDiscount) {}
 
-  public handle = async(): Promise<Response> => {
+  public handle = async(request: GetProductsWithDiscountController.Request): Promise<Response> => {
     try {
-      const products = await this.getProductsWithDiscount.getWithDiscount()
+      const page = Number(request.page) || DEFAULT_PAGE
+      const limit = Number(request.limit) || DEFAULT_LIMIT
+      const products = await this.getProductsWithDiscount.getWithDiscount(page, limit)
       return HttpHelper.ok(products)
     } catch (error) {
       return HttpHelper.serverError(error)
     }
+  }
+}
+
+export namespace GetProductsWithDiscountController {
+  export interface Request {
+    page?: string
+    limit?: string
   }
 }
