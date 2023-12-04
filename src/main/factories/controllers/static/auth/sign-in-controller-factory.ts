77f9b08c42d2805dd54634e1e40e16de1c94923d
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-12-04 14:17:39 -0300

    refactor: change AuthenticationFactory to a class

--- src/main/factories/controllers/static/auth/sign-in-controller-factory.ts
+++ src/main/factories/controllers/static/auth/sign-in-controller-factory.ts
@@ -1,12 +1,12 @@
 import { makeSignInValidation } from '@/main/factories/validations/auth'
-import { makeDbAuthentication } from '@/main/factories/usecases/static/auth'
+import { AuthenticationFactory } from '@/main/factories/usecases/static/auth'
 import { LogControllerDecoratorFactory } from '@/main/factories/decorators/log'
 import { type Controller } from '@/presentation/protocols'
 import { SignInController } from '@/presentation/controllers/dynamic/auth'
 
 export class SignInControllerFactory {
   public static makeSignInController = (): Controller => {
-    const controller = new SignInController(makeSignInValidation(), makeDbAuthentication())
+    const controller = new SignInController(makeSignInValidation(), AuthenticationFactory.makeAuthentication())
     return LogControllerDecoratorFactory.makeLogControllerDecorator(controller)
   }
 }
--- src/main/factories/usecases/static/auth/authentication-factory.ts
+++ src/main/factories/usecases/static/auth/authentication-factory.ts
@@ -5,11 +5,13 @@ import { BcryptAdapter, JwtAdapter } from '@/infra/cryptography'
 import { StaticAccountMongoRepository } from '@/infra/db/mongodb/static/auth'
 import { DynamicAccountMongoRepository } from '@/infra/db/mongodb/dynamic/auth'
 
-export const makeDbAuthentication = (): Authentication => {
-  const salt = 12
-  const staticAccountMongoRepository = new StaticAccountMongoRepository()
-  const dynamicAccountMongoRepository = new DynamicAccountMongoRepository()
-  const bcryptAdapter = new BcryptAdapter(salt)
-  const jwtAdapter = new JwtAdapter(env.jwtSecret)
-  return new DbAuthentication(staticAccountMongoRepository, bcryptAdapter, jwtAdapter, dynamicAccountMongoRepository)
+export class AuthenticationFactory {
+  public static makeAuthentication = (): Authentication => {
+    const salt = 12
+    const staticAccountMongoRepository = new StaticAccountMongoRepository()
+    const dynamicAccountMongoRepository = new DynamicAccountMongoRepository()
+    const bcryptAdapter = new BcryptAdapter(salt)
+    const jwtAdapter = new JwtAdapter(env.jwtSecret)
+    return new DbAuthentication(staticAccountMongoRepository, bcryptAdapter, jwtAdapter, dynamicAccountMongoRepository)
+  }
 }
