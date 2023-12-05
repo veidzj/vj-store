1f401f2cd1a6def47d63beb3dbfa31afb28be0a2
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-12-05 13:41:16 -0300

    feat: ensure DbGetProductsByCategory calls GetProductsByCategoryRepository with correct sortBy value

--- src/application/protocols/db/static/product/get-products-by-category-repository.ts
+++ src/application/protocols/db/static/product/get-products-by-category-repository.ts
@@ -1,7 +1,7 @@
 import { type Product } from '@/domain/models'
 
 export interface GetProductsByCategoryRepository {
-  getByCategory: (category: string, page: number, limit: number) => Promise<GetProductsByCategoryRepository.Output>
+  getByCategory: (category: string, page: number, limit: number, sortBy?: string) => Promise<GetProductsByCategoryRepository.Output>
 }
 
 export namespace GetProductsByCategoryRepository {
