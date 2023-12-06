c1d39999e83150dd7b4aef67df0b28c8d3f40e84
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-12-06 18:07:19 -0300

    feat: add GetLatestProducts route

--- src/main/factories/controllers/static/product/index.ts
+++ src/main/factories/controllers/static/product/index.ts
@@ -1,3 +1,4 @@
+export * from './get-latest-products-controller-factory'
 export * from './get-product-by-slug-controller-factory'
 export * from './get-products-by-category-controller-factory'
 export * from './get-products-with-discount-controller-factory'
