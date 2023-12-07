138b6d95bc3ed0fcb5ac262caf84744225209cb1
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-12-07 12:32:23 -0300

    refactor: move http components docs to separate folder

--- src/main/docs/components.ts
+++ src/main/docs/components.ts
@@ -1,7 +1,4 @@
-import { badRequest } from './components/bad-request'
-import { forbidden } from './components/forbidden'
-import { serverError } from './components/server-error'
-import { unauthorized } from './components/unauthorized'
+import { badRequest, unauthorized, forbidden, serverError } from '@/main/docs/components/http'
 
 export default {
   badRequest,
