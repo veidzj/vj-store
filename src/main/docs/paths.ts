83f7d3e245adf47a17e57adc2114909bf27f0479
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-12-07 20:13:31 -0300

    docs: add swagger get product by slug

--- src/main/docs/paths.ts
+++ src/main/docs/paths.ts
@@ -1,11 +1,12 @@
 import { signUpPath, signInPath } from '@/main/docs/paths/auth'
 import { categoryPath } from '@/main/docs/paths/category'
-import { getProductsByCategoryPath, getProductsWithDiscountPath, getLatestProductsPath } from '@/main/docs/paths/product'
+import { getProductBySlugPath, getProductsByCategoryPath, getProductsWithDiscountPath, getLatestProductsPath } from '@/main/docs/paths/product'
 
 export default {
   '/signup': signUpPath,
   '/signin': signInPath,
   '/category': categoryPath,
+  '/product/slug/{slug}': getProductBySlugPath,
   '/product/category/{categoryName}': getProductsByCategoryPath,
   '/product/discount': getProductsWithDiscountPath,
   '/product/latest': getLatestProductsPath
---
+++ src/main/docs/paths/product/get-product-by-slug-path.ts
@@ -0,0 +1,32 @@
+export const getProductBySlugPath = {
+  get: {
+    tags: ['Product'],
+    summary: 'List one product by slug',
+    parameters: [{
+      in: 'path',
+      name: 'slug',
+      required: true,
+      schema: {
+        type: 'string'
+      }
+    }],
+    responses: {
+      200: {
+        description: 'Success',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/product'
+            }
+          }
+        }
+      },
+      400: {
+        $ref: '#/components/badRequest'
+      },
+      500: {
+        $ref: '#/components/serverError'
+      }
+    }
+  }
+}
