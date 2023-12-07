6f8ac4a7023091f7d68ff47387cc2f632cf53ccf
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-12-07 20:06:22 -0300

    docs: add swagger get products with discount

--- src/main/docs/paths.ts
+++ src/main/docs/paths.ts
@@ -1,10 +1,11 @@
 import { signUpPath, signInPath } from '@/main/docs/paths/auth'
 import { categoryPath } from '@/main/docs/paths/category'
-import { productByCategoryPath } from '@/main/docs/paths/product'
+import { productByCategoryPath, productsWithDiscountPath } from '@/main/docs/paths/product'
 
 export default {
   '/signup': signUpPath,
   '/signin': signInPath,
   '/category': categoryPath,
-  '/product/category/{categoryName}': productByCategoryPath
+  '/product/category/{categoryName}': productByCategoryPath,
+  '/product/discount': productsWithDiscountPath
 }
--- src/main/docs/paths/product/index.ts
+++ src/main/docs/paths/product/index.ts
@@ -1,1 +1,2 @@
 export * from './product-by-category-path'
+export * from './products-with-discount-path'
