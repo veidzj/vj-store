0ec3f045d5fe3e6859bea4b6cfb115ed6d02292c
veidzj <veidzjv@gmail.com>
2023-12-25 00:39:01 -0300

    refactor: move sut creation to a factory

--- tests/domain/entities/account/account.spec.ts
+++ tests/domain/entities/account/account.spec.ts
@@ -3,6 +3,14 @@ import { faker } from '@faker-js/faker'
 
 import { Account } from '@/domain/entities/account'
 
+let username: string
+let email: string
+let password: string
+
+const makeSut = (): Account => {
+  return new Account(username, email, password)
+}
+
 describe('Account Entity', () => {
   beforeAll(() => {
     MockDate.set(new Date())
@@ -12,12 +20,15 @@ describe('Account Entity', () => {
     MockDate.reset()
   })
 
+  beforeEach(() => {
+    username = faker.internet.userName()
+    email = faker.internet.email()
+    password = faker.internet.password()
+  })
+
   test('Should instantiate with correct values', () => {
     const currentDate = new Date()
-    const username = faker.internet.userName()
-    const email = faker.internet.email()
-    const password = faker.internet.password()
-    const sut = new Account(username, email, password)
+    const sut = makeSut()
 
     expect(sut.getId()).toBeTruthy()
     expect(sut.getUsername()).toBe(username)
