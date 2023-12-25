430c00f1fde81ad629a7ed6b16f5589f884afc7e
veidzj <veidzjv@gmail.com>
2023-12-24 22:29:57 -0300

    test: ensure Account instantiates with correct values

---
+++ tests/domain/entities/account/account.spec.ts
@@ -0,0 +1,25 @@
+import MockDate from 'mockdate'
+
+import { Account } from '@/domain/entities/account'
+
+describe('Account Entity', () => {
+  beforeAll(() => {
+    MockDate.set(new Date())
+  })
+
+  afterAll(() => {
+    MockDate.reset()
+  })
+
+  test('Should instantiate with correct values', () => {
+    const currentDate = new Date()
+    const sut = new Account('any_username', 'any_email@mail.com', 'any_password')
+
+    expect(sut.getId()).toBeTruthy()
+    expect(sut.getUsername()).toBe('any_username')
+    expect(sut.getEmail()).toBe('any_email@mail.com')
+    expect(sut.getPassword()).toBe('any_password')
+    expect(sut.getIsActive()).toBe(true)
+    expect(sut.getCreatedAt()).toEqual(currentDate)
+  })
+})
