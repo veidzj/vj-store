521a83dc0fe980ffba344ffec2633927835854a6
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-12-27 02:41:46 -0300

    feat: ensure AddAccountRepository throws if mongo throws

---
+++ src/infra/db/mongodb/account/account-mongo-repository.ts
@@ -0,0 +1,11 @@
+import { type AddAccountRepository } from '@/application/protocols/account'
+import { type MongoHelper } from '@/infra/db/mongodb'
+
+export class AccountMongoRepository implements AddAccountRepository {
+  private readonly mongoHelper: MongoHelper
+
+  public async add(input: AddAccountRepository.Input): Promise<void> {
+    const accountCollection = this.mongoHelper.getCollection('accounts')
+    await accountCollection.insertOne({ ...input })
+  }
+}
---
+++ src/infra/db/mongodb/mongo-helper.ts
@@ -0,0 +1,36 @@
+import { MongoClient, type Collection } from 'mongodb'
+
+export class MongoHelper {
+  private static instance: MongoHelper
+  private client: MongoClient | null = null
+  private uri: string | null = null
+
+  private constructor() {}
+
+  public static getInstance(): MongoHelper {
+    if (!MongoHelper.instance) {
+      MongoHelper.instance = new MongoHelper()
+    }
+
+    return MongoHelper.instance
+  }
+
+  public async connect(uri: string): Promise<void> {
+    this.uri = uri
+    this.client = await MongoClient.connect(uri)
+  }
+
+  public async disconnect(): Promise<void> {
+    if (this.client) {
+      await this.client.close()
+      this.client = null
+    }
+  }
+
+  public getCollection(name: string): Collection {
+    if (!this.client) {
+      throw new Error('No active connection to the database')
+    }
+    return this.client.db().collection(name)
+  }
+}
