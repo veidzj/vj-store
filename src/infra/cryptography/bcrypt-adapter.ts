7eb6313763cb4f7c815acc3f24b5d0ae04786f81
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-12-27 17:06:52 -0300

    feat: ensure BcryptAdapter calls hash with correct values

---
+++ src/infra/cryptography/bcrypt-adapter.ts
@@ -0,0 +1,11 @@
+import bcrypt from 'bcrypt'
+import { type Hasher } from '@/application/protocols/cryptography'
+
+export class BcryptAdapter implements Hasher {
+  constructor(private readonly salt: number) {}
+
+  public async hash(plainText: string): Promise<string> {
+    const hashedValue = await bcrypt.hash(plainText, this.salt)
+    return hashedValue
+  }
+}
