c18ea6beee0b922c2e96a91c14bab31a49359a13
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-12-28 17:12:33 -0300

    test: ensure SignUpController returns badRequest if AddAccount throws EntityValidationError

--- tests/presentation/controllers/account/sign-up-controller.spec.ts
+++ tests/presentation/controllers/account/sign-up-controller.spec.ts
@@ -1,6 +1,10 @@
-import { SignUpController } from '@/presentation/controllers/account'
-import { mockAddAccountInput } from '@/tests/domain/mocks/account'
+import { faker } from '@faker-js/faker'
+
 import { AddAccountSpy, AuthenticationSpy } from '@/tests/presentation/mocks/account'
+import { mockAddAccountInput } from '@/tests/domain/mocks/account'
+import { SignUpController } from '@/presentation/controllers/account'
+import { EntityValidationError } from '@/domain/errors'
+import { HttpHelper } from '@/presentation/helpers'
 
 interface Sut {
   sut: SignUpController
@@ -27,6 +31,16 @@ describe('SignUpController', () => {
     const request = mockRequest()
     await sut.handle(request)
     expect(addAccountSpy.input).toEqual(request)
+  })
+
+  test('Should return badRequest if AddAccount throws EntityValidationError', async() => {
+    const { sut, addAccountSpy } = makeSut()
+    const errorMessage = faker.word.words()
+    jest.spyOn(addAccountSpy, 'add').mockImplementationOnce(() => {
+      throw new EntityValidationError(errorMessage)
+    })
+    const response = await sut.handle(mockRequest())
+    expect(response).toEqual(HttpHelper.badRequest(new EntityValidationError(errorMessage)))
   })
 
   test('Should call Authentication with correct values', async() => {
