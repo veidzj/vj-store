f84559a66c30ae85e3fc9e2b1c9c41ba7d7af75e
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-12-28 23:58:47 -0300

    feat: ensure DbAuthentication calls Encrypter with correct id

--- src/application/usecases/account/db-authentication.ts
+++ src/application/usecases/account/db-authentication.ts
@@ -1,12 +1,13 @@
 import { type GetAccountByEmailRepository } from '@/application/protocols/account/queries'
-import { type HashComparer } from '@/application/protocols/cryptography'
+import { type HashComparer, type Encrypter } from '@/application/protocols/cryptography'
 import { AccountNotFoundError, InvalidCredentialsError } from '@/domain/errors/account'
 import { type Authentication } from '@/domain/usecases/account'
 
 export class DbAuthentication implements Authentication {
   constructor(
     private readonly getAccountByEmailRepository: GetAccountByEmailRepository,
-    private readonly hashComparer: HashComparer
+    private readonly hashComparer: HashComparer,
+    private readonly encrypter: Encrypter
   ) {}
 
   public async auth(input: Authentication.Input): Promise<string> {
@@ -19,6 +20,8 @@ export class DbAuthentication implements Authentication {
     if (!isMatch) {
       throw new InvalidCredentialsError()
     }
+
+    await this.encrypter.encrypt(account.getId())
     return await Promise.resolve('')
   }
 }
