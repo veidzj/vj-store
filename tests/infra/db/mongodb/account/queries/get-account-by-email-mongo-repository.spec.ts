ae3cb68b31e9d24fae84057b3401934904ae1324
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-12-29 02:15:09 -0300

    test: ensure GetAccountByEmailMongoRepository throws if mongo throws

---
+++ tests/infra/db/mongodb/account/queries/get-account-by-email-mongo-repository.spec.ts
@@ -0,0 +1,36 @@
+import { Collection } from 'mongodb'
+import { faker } from '@faker-js/faker'
+
+import { MongoHelper } from '@/infra/db/mongodb'
+import { GetAccountByEmailMongoRepository } from '@/infra/db/mongodb/account/queries'
+import { env } from '@/main/config'
+
+let accountCollection: Collection
+
+const makeSut = (): GetAccountByEmailMongoRepository => {
+  return new GetAccountByEmailMongoRepository()
+}
+
+describe('GetAccountByEmailMongoRepository', () => {
+  const mongoHelper: MongoHelper = MongoHelper.getInstance()
+
+  beforeAll(async() => {
+    await mongoHelper.connect(env.mongoUrl)
+  })
+
+  afterAll(async() => {
+    await mongoHelper.disconnect()
+  })
+
+  beforeEach(async() => {
+    accountCollection = mongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
+  test('Should throw if mongo throws', async() => {
+    const sut = makeSut()
+    jest.spyOn(Collection.prototype, 'findOne').mockImplementationOnce(() => { throw new Error() })
+    const promise = sut.getByEmail(faker.internet.email())
+    await expect(promise).rejects.toThrow()
+  })
+})
