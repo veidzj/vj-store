d94d51546b4286c0d9e62153aeafbc46c0f93437
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-12-29 01:13:18 -0300

    refactor: move sut creation to a factory

--- tests/infra/cryptography/jwt-adapter.spec.ts
+++ tests/infra/cryptography/jwt-adapter.spec.ts
@@ -6,10 +6,14 @@ import { JwtAdapter } from '@/infra/cryptography'
 const jwtSecret: string = faker.string.uuid()
 const plainText: string = faker.string.uuid()
 
+const makeSut = (): JwtAdapter => {
+  return new JwtAdapter(jwtSecret)
+}
+
 describe('JwtAdapter', () => {
   describe('Encrypter', () => {
     test('Should call sign with correct values', async() => {
-      const sut = new JwtAdapter(jwtSecret)
+      const sut = makeSut()
       const signSpy = jest.spyOn(jwt, 'sign')
       await sut.encrypt(plainText)
       expect(signSpy).toHaveBeenCalledWith({ id: plainText }, jwtSecret, { expiresIn: '1h' })
