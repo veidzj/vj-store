394bfe49a4b76d731fb83eba492c5c750f1a5bfe
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-12-29 01:46:18 -0300

    feat: ensure UpdateAccessTokenMongoRepository throws if mongo throws

--- src/infra/db/mongodb/account/commands/index.ts
+++ src/infra/db/mongodb/account/commands/index.ts
@@ -1,1 +1,2 @@
 export * from './add-account-mongo-repository'
+export * from './update-access-token-mongo-repository'
---
+++ src/infra/db/mongodb/account/commands/update-access-token-mongo-repository.ts
@@ -0,0 +1,17 @@
+import { MongoHelper } from '@/infra/db/mongodb'
+import { type UpdateAccessTokenRepository } from '@/application/protocols/account/commands'
+
+export class UpdateAccessTokenMongoRepository implements UpdateAccessTokenRepository {
+  private readonly mongoHelper: MongoHelper = MongoHelper.getInstance()
+
+  public async updateAccessToken(input: UpdateAccessTokenRepository.Input): Promise<void> {
+    const accountCollection = this.mongoHelper.getCollection('accounts')
+    await accountCollection.updateOne({
+      id: input.id
+    }, {
+      $set: {
+        accessToken: input.accessToken
+      }
+    })
+  }
+}
