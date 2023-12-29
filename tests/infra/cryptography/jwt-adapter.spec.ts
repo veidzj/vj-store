f9adfed2065cf69f9c5e251ead531faefa4e5558
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-12-29 01:12:37 -0300

    refactor: change test values to faker

--- tests/infra/cryptography/jwt-adapter.spec.ts
+++ tests/infra/cryptography/jwt-adapter.spec.ts
@@ -1,14 +1,18 @@
 import jwt from 'jsonwebtoken'
+import { faker } from '@faker-js/faker'
 
 import { JwtAdapter } from '@/infra/cryptography'
+
+const jwtSecret: string = faker.string.uuid()
+const plainText: string = faker.string.uuid()
 
 describe('JwtAdapter', () => {
   describe('Encrypter', () => {
     test('Should call sign with correct values', async() => {
-      const sut = new JwtAdapter('jwt_secret')
+      const sut = new JwtAdapter(jwtSecret)
       const signSpy = jest.spyOn(jwt, 'sign')
-      await sut.encrypt('plainText')
-      expect(signSpy).toHaveBeenCalledWith({ id: 'plainText' }, 'jwt_secret', { expiresIn: '1h' })
+      await sut.encrypt(plainText)
+      expect(signSpy).toHaveBeenCalledWith({ id: plainText }, jwtSecret, { expiresIn: '1h' })
     })
   })
 })
