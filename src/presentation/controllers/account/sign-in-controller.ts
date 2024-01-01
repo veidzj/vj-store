716d4e238862fe2a632c45b3cc30e84334c458df
veidzj <veidzjv@gmail.com>
2024-01-01 01:08:53 -0300

    feat: ensure SignInController returns notFound if Authentication throws AccountNotFoundError

--- src/presentation/controllers/account/sign-in-controller.ts
+++ src/presentation/controllers/account/sign-in-controller.ts
@@ -1,13 +1,21 @@
 import { type Controller, type Response } from '@/presentation/protocols'
 import { HttpHelper } from '@/presentation/helpers'
 import { type Authentication } from '@/domain/usecases/account'
+import { AccountNotFoundError } from '@/domain/errors/account'
 
 export class SignInController implements Controller {
   constructor(private readonly authentication: Authentication) {}
 
   public async handle(request: SignInController.Request): Promise<Response> {
-    await this.authentication.auth(request)
-    return HttpHelper.ok({})
+    try {
+      await this.authentication.auth(request)
+      return HttpHelper.ok({})
+    } catch (error) {
+      if (error instanceof AccountNotFoundError) {
+        return HttpHelper.notFound(error)
+      }
+      return HttpHelper.ok({})
+    }
   }
 }
 
