e701ad5ccac92ea0d69333f7af354154964001bd
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-01-02 11:44:53 -0300

    refactor: move sut creation to a factory

--- tests/presentation/middlewares/auth-middleware.spec.ts
+++ tests/presentation/middlewares/auth-middleware.spec.ts
@@ -1,10 +1,21 @@
-import { AutheMiddleware } from '@/presentation/middlewares'
+import { AuthMiddleware } from '@/presentation/middlewares'
 import { HttpHelper } from '@/presentation/helpers'
 import { InvalidCredentialsError } from '@/domain/errors/account'
 
+interface Sut {
+  sut: AuthMiddleware
+}
+
+const makeSut = (): Sut => {
+  const sut = new AuthMiddleware()
+  return {
+    sut
+  }
+}
+
 describe('AuthMiddleware', () => {
   test('Should return unauthorized if accessToken is not provided', async() => {
-    const sut = new AutheMiddleware()
+    const { sut } = makeSut()
     const response = await sut.handle({})
     expect(response).toEqual(HttpHelper.unauthorized(new InvalidCredentialsError()))
   })
