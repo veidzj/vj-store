8b53939c1fb93c2844e778d90426dcf1e7b2d916
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-01-02 15:32:42 -0300

    feat: ensure DbGetAccountIdByToken throws TokenError if Decrypter throws

--- src/application/usecases/account/db-get-account-id-by-token.ts
+++ src/application/usecases/account/db-get-account-id-by-token.ts
@@ -1,11 +1,16 @@
 import { type Decrypter } from '@/application/protocols/cryptography'
 import { type GetAccountIdByToken } from '@/domain/usecases/account'
+import { TokenError } from '@/domain/errors/account'
 
 export class DbGetAccountIdByToken implements GetAccountIdByToken {
   constructor(private readonly decrypter: Decrypter) {}
 
   public async getByToken(accessToken: string, role: string): Promise<string> {
-    await this.decrypter.decrypt(accessToken)
+    try {
+      await this.decrypter.decrypt(accessToken)
+    } catch (error) {
+      throw new TokenError()
+    }
     return await Promise.resolve('')
   }
 }
