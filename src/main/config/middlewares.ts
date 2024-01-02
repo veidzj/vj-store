31c4eed5ac9a398dfb6ad5aa41201f80ebfef395
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-01-02 01:07:15 -0300

    feat: ensure app returns default content type as json

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,9 +1,10 @@
 import { type Express } from 'express'
 
-import { bodyParser } from '@/main/middlewares'
+import { bodyParser, contentType } from '@/main/middlewares'
 
 export class Middlewares {
   public static setup = (app: Express): void => {
     app.use(bodyParser)
+    app.use(contentType)
   }
 }
