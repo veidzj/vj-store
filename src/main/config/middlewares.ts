be4aa964e90e81028b3e341b4175962f1e45a02d
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-01-02 03:07:00 -0300

    feat: ensure app enables CORS

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,10 +1,11 @@
 import { type Express } from 'express'
 
-import { bodyParser, contentType } from '@/main/middlewares'
+import { bodyParser, contentType, cors } from '@/main/middlewares'
 
 export class Middlewares {
   public static setup = (app: Express): void => {
     app.use(bodyParser)
     app.use(contentType)
+    app.use(cors)
   }
 }
