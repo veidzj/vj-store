f247bc3f79873ae8ab3c9f95837f10718157a1e3
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-01-02 17:21:37 -0300

    docs: add apiKeyAuthSchema for swagger

--- src/main/docs/components.ts
+++ src/main/docs/components.ts
@@ -1,6 +1,10 @@
+import { apiKeyAuthSchema } from '@/main/docs/schemas/account'
 import { badRequest, unauthorized, notFound, conflict, serverError } from '@/main/docs/components/http'
 
 export default {
+  securitySchemes: {
+    apiKeyAuth: apiKeyAuthSchema
+  },
   badRequest,
   unauthorized,
   notFound,
