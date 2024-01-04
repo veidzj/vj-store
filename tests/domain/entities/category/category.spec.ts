b3548a82a39308968fd92a45c43bdc2003545f53
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-01-04 20:43:58 -0300

    refactor: move sut creation to a factory

--- tests/domain/entities/category/category.spec.ts
+++ tests/domain/entities/category/category.spec.ts
@@ -1,11 +1,24 @@
+import { faker } from '@faker-js/faker'
+
 import { Category } from '@/domain/entities/category'
 
+let name: string
+
+const makeSut = (): Category => {
+  return new Category(name)
+}
+
 describe('Category Entity', () => {
+  beforeEach(() => {
+    const randomString = faker.string.alpha({ length: { min: 4, max: 19 } })
+    name = faker.string.alpha({ length: 1, casing: 'upper' }) + randomString
+  })
+
   test('Should instantiate with correct values', () => {
     const currentDate = new Date()
-    const sut = new Category('any_name')
+    const sut = makeSut()
     expect(sut.getId()).toBeTruthy()
-    expect(sut.getName()).toBe('any_name')
+    expect(sut.getName()).toBe(name)
     expect(sut.getCreatedAt()).toEqual(currentDate)
     expect(sut.getUpdateHistory()).toEqual([])
   })
