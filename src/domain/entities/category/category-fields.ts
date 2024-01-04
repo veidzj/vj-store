123b84bdb741f35ec8d5bf8e3c9f5e1612069cc0
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-01-04 17:26:03 -0300

    feat: ensure Category instantiates with correct values

---
+++ src/domain/entities/category/category-fields.ts
@@ -0,0 +1,3 @@
+export enum CategoryFields {
+  name = 'name'
+}
---
+++ src/domain/entities/category/category.ts
@@ -0,0 +1,40 @@
+import { type UpdateLog } from '@/domain/common'
+import { AggregateRoot } from '@/domain/seedwork'
+import { type CategoryFields } from '@/domain/entities/category'
+
+export class Category extends AggregateRoot {
+  private name: string
+  private updateHistory: UpdateLog<CategoryFields> | [] = []
+
+  constructor(name: string) {
+    super()
+    this.setName(name)
+  }
+
+  public getId(): string {
+    return this.id
+  }
+
+  public getName(): string {
+    return this.name
+  }
+
+  public getCreatedAt(): Date {
+    return this.createdAt
+  }
+
+  public getUpdateHistory(): UpdateLog<CategoryFields> | [] {
+    return this.updateHistory
+  }
+
+  public setName(name: string): void {
+    this.name = name
+  }
+
+  public setUpdateHistory(fields: CategoryFields[]): void {
+    this.updateHistory = {
+      fields,
+      updatedAt: new Date()
+    }
+  }
+}
