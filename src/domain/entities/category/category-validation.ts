ea4c59c6f7334672ccd141bb3eba5477221596da
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-01-05 01:49:59 -0300

    feat: ensure Category throws if name is more than 20 characters long

--- src/domain/entities/category/category-validation.ts
+++ src/domain/entities/category/category-validation.ts
@@ -9,5 +9,8 @@ export class CategoryValidation {
     if (name.length < 3) {
       throw new EntityValidationError('Name must be at least 3 characters long')
     }
+    if (name.length > 20) {
+      throw new EntityValidationError('Name must be less than or equal to 20 characters long')
+    }
   }
 }
