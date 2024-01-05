d775f02dc9e072146d8189b9723848221360ed33
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-01-05 18:27:38 -0300

    feat: ensure AddCategoryController returns ok on success

--- src/presentation/controllers/category/add-category-controller.ts
+++ src/presentation/controllers/category/add-category-controller.ts
@@ -1,10 +1,13 @@
+import { type Controller, type Response } from '@/presentation/protocols'
+import { HttpHelper } from '@/presentation/helpers'
 import { type AddCategory } from '@/domain/usecases/category'
 
-export class AddCategoryController {
+export class AddCategoryController implements Controller {
   constructor(private readonly addCategory: AddCategory) {}
 
-  public async handle(request: AddCategoryController.Request): Promise<void> {
+  public async handle(request: AddCategoryController.Request): Promise<Response> {
     await this.addCategory.add({ name: request.name })
+    return HttpHelper.ok({ message: 'Category successfully added' })
   }
 }
 
