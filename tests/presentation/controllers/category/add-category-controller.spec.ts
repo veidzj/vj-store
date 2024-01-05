6d3a3599300da742912b6af3c785e3a8a5e4dcf7
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-01-05 18:32:24 -0300

    test: ensure AddCategoryController returns badRequest if AddCategory throws EntityValidationError

--- tests/presentation/controllers/category/add-category-controller.spec.ts
+++ tests/presentation/controllers/category/add-category-controller.spec.ts
@@ -1,8 +1,11 @@
+import { faker } from '@faker-js/faker'
+
+import { throwError } from '@/tests/test-helper'
+import { AddCategorySpy } from '@/tests/presentation/mocks/category'
+import { mockAddCategoryInput } from '@/tests/domain/mocks/category'
 import { AddCategoryController } from '@/presentation/controllers/category'
 import { HttpHelper } from '@/presentation/helpers'
-import { mockAddCategoryInput } from '@/tests/domain/mocks/category'
-import { AddCategorySpy } from '@/tests/presentation/mocks/category'
-import { throwError } from '@/tests/test-helper'
+import { EntityValidationError } from '@/domain/errors'
 
 interface Sut {
   sut: AddCategoryController
@@ -26,6 +29,16 @@ describe('AddCategoryController', () => {
     const request = mockRequest()
     await sut.handle(request)
     expect(addCategorySpy.input.name).toBe(request.name)
+  })
+
+  test('Should return badRequest if AddCategory throws EntityValidationError', async() => {
+    const { sut, addCategorySpy } = makeSut()
+    const errorMessage = faker.word.words()
+    jest.spyOn(addCategorySpy, 'add').mockImplementationOnce(() => {
+      throw new EntityValidationError(errorMessage)
+    })
+    const response = await sut.handle(mockRequest())
+    expect(response).toEqual(HttpHelper.badRequest(new EntityValidationError(errorMessage)))
   })
 
   test('Should return serverError if AddCategory throws', async() => {
