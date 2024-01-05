edf26defc3b012a428c6837283a73ad00d8e7634
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-01-05 18:51:49 -0300

    feat: ensure Category returns conflict if AddCategory throws CategoryAlreadyExistsError

--- src/presentation/controllers/category/add-category-controller.ts
+++ src/presentation/controllers/category/add-category-controller.ts
@@ -2,6 +2,7 @@ import { type Controller, type Response } from '@/presentation/protocols'
 import { HttpHelper } from '@/presentation/helpers'
 import { type AddCategory } from '@/domain/usecases/category'
 import { EntityValidationError } from '@/domain/errors'
+import { CategoryAlreadyExistsError } from '@/domain/errors/category'
 
 export class AddCategoryController implements Controller {
   constructor(private readonly addCategory: AddCategory) {}
@@ -13,6 +14,9 @@ export class AddCategoryController implements Controller {
     } catch (error) {
       if (error instanceof EntityValidationError) {
         return HttpHelper.badRequest(error)
+      }
+      if (error instanceof CategoryAlreadyExistsError) {
+        return HttpHelper.conflict(error)
       }
       return HttpHelper.serverError(error as Error)
     }
