a29b234c685887889de67400f58c32086420a530
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-01-05 20:07:57 -0300

    feat: ensure DbAddCategory calls CheckCategoryByNameRepositorySpy with correct name

--- src/application/usecases/category/commands/db-add-category.ts
+++ src/application/usecases/category/commands/db-add-category.ts
@@ -1,12 +1,17 @@
+import { type CheckCategoryByNameRepository } from '@/application/protocols/category/queries'
 import { type AddCategoryRepository } from '@/application/protocols/category/commands'
 import { Category } from '@/domain/entities/category'
 import { type AddCategory } from '@/domain/usecases/category'
 
 export class DbAddCategory implements AddCategory {
-  constructor(private readonly addCategoryRepository: AddCategoryRepository) {}
+  constructor(
+    private readonly checkCategoryByNameRepository: CheckCategoryByNameRepository,
+    private readonly addCategoryRepository: AddCategoryRepository
+  ) {}
 
   public async add(input: AddCategory.Input): Promise<void> {
     const category = new Category(input.name)
+    await this.checkCategoryByNameRepository.checkByName(category.getName())
     const addCategoryRepositoryInput = this.makeAddAccountRepositoryInput(category)
     await this.addCategoryRepository.add(addCategoryRepositoryInput)
   }
