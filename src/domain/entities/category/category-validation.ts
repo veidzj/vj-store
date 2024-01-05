8bb430bb4b24a62c519768788a412970d86abec3
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-01-04 22:44:41 -0300

    feat: ensure Category throws if name starts with a lowercase letter

--- src/domain/entities/category/category-validation.ts
+++ src/domain/entities/category/category-validation.ts
@@ -1,7 +1,11 @@
+import isUppercase from 'validator/lib/isUppercase'
 import { EntityValidationError } from '@/domain/errors'
 
 export class CategoryValidation {
   public static validateName(name: string): void {
+    if (!isUppercase(name[0])) {
+      throw new EntityValidationError('Name must start with an uppercase letter')
+    }
     if (name.length < 3) {
       throw new EntityValidationError('Name must be at least 3 characters long')
     }
