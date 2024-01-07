ad9570c92385d61acb814aa0a5df84fecff775d0
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-01-07 16:17:15 -0300

    refactor: remove test factory to use mock

--- tests/infra/db/mongodb/category/queries/check-account-by-email-mongo-repository.spec.ts
+++ tests/infra/db/mongodb/category/queries/check-account-by-email-mongo-repository.spec.ts
@@ -1,21 +1,15 @@
 import { Collection } from 'mongodb'
-import { faker } from '@faker-js/faker'
 
 import { throwError } from '@/tests/test-helper'
 import { connectToDatabase, disconnectFromDatabase, clearCollection } from '@/tests/infra/db/mongodb'
 import { getCategoryCollection } from '@/tests/infra/db/mongodb/category'
-import { CheckCategoryByNameMongoRepository } from '@/infra/db/mongodb/category/queries'
 import { mockAddCategoryRepositoryInput } from '@/tests/application/mocks/category/commands'
+import { CheckCategoryByNameMongoRepository } from '@/infra/db/mongodb/category/queries'
 
 let categoryCollection: Collection
 
 const makeSut = (): CheckCategoryByNameMongoRepository => {
   return new CheckCategoryByNameMongoRepository()
-}
-
-const makeName = (min: number = 4, max: number = 19): string => {
-  const randomString = faker.string.alpha({ length: { min, max }, casing: 'lower' })
-  return faker.string.alpha({ length: 1, casing: 'upper' }) + randomString
 }
 
 describe('CheckCategoryByNameMongoRepository', () => {
@@ -36,7 +30,7 @@ describe('CheckCategoryByNameMongoRepository', () => {
 
   test('Should throw if mongo throws', async() => {
     jest.spyOn(Collection.prototype, 'countDocuments').mockImplementationOnce(throwError)
-    const promise = sut.checkByName(makeName())
+    const promise = sut.checkByName(mockAddCategoryRepositoryInput().name)
     await expect(promise).rejects.toThrow()
   })
 
@@ -48,7 +42,7 @@ describe('CheckCategoryByNameMongoRepository', () => {
   })
 
   test('Should return false if name does not exists', async() => {
-    const categoryExists = await sut.checkByName(makeName())
+    const categoryExists = await sut.checkByName(mockAddCategoryRepositoryInput().name)
     expect(categoryExists).toBe(false)
   })
 })
