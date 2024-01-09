81f70b67fe92f9d73cdc6a48ac80a1f46af6aa72
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-01-09 05:28:17 -0300

    feat: ensure GetAllCategories returns serverError if GetAllCategories throws

--- src/presentation/controllers/category/get-all-categories-controller.ts
+++ src/presentation/controllers/category/get-all-categories-controller.ts
@@ -1,9 +1,16 @@
 import { type GetAllCategories } from '@/domain/usecases/category/queries'
+import { HttpHelper } from '@/presentation/helpers'
+import { type Controller, type Response } from '@/presentation/protocols'
 
-export class GetAllCategoriesController {
+export class GetAllCategoriesController implements Controller {
   constructor(private readonly getAllCategories: GetAllCategories) {}
 
-  public async handle(): Promise<void> {
-    await this.getAllCategories.getAll()
+  public async handle(): Promise<Response> {
+    try {
+      await this.getAllCategories.getAll()
+      return HttpHelper.ok({})
+    } catch (error) {
+      return HttpHelper.serverError(error as Error)
+    }
   }
 }
