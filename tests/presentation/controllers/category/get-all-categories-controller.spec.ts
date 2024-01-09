dc5f837148fa0bfe8bcc5bb051f33177019f3c9d
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-01-09 05:29:47 -0300

    test: ensure GetAllCategories returns serverError if GetAllCategories throws

--- tests/presentation/controllers/category/get-all-categories-controller.spec.ts
+++ tests/presentation/controllers/category/get-all-categories-controller.spec.ts
@@ -1,5 +1,7 @@
+import { throwError } from '@/tests/test-helper'
 import { GetAllCategoriesSpy } from '@/tests/presentation/mocks/category'
 import { GetAllCategoriesController } from '@/presentation/controllers/category'
+import { HttpHelper } from '@/presentation/helpers'
 
 interface Sut {
   sut: GetAllCategoriesController
@@ -21,5 +23,12 @@ describe('GetAllCategoriesController', () => {
     jest.spyOn(getAllCategoriesSpy, 'getAll')
     await sut.handle()
     expect(getAllCategoriesSpy.getAll).toHaveBeenCalledTimes(1)
+  })
+
+  test('Should return serverError if GetAllCategories throws', async() => {
+    const { sut, getAllCategoriesSpy } = makeSut()
+    jest.spyOn(getAllCategoriesSpy, 'getAll').mockImplementationOnce(throwError)
+    const response = await sut.handle()
+    expect(response).toEqual(HttpHelper.serverError(new Error()))
   })
 })
