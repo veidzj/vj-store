9c22c6ed281bc0389cf691fedbb3ad437a06b651
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-01-10 07:34:49 -0300

    feat: add PUT /category route

--- src/main/routes/category-route.ts
+++ src/main/routes/category-route.ts
@@ -1,10 +1,11 @@
 import { type Router } from 'express'
 
 import { ExpressRouteAdapter } from '@/main/adapters'
-import { AddCategoryControllerFactory, GetAllCategoriesControllerFactory } from '@/main/factories/controllers/category'
+import { AddCategoryControllerFactory, GetAllCategoriesControllerFactory, UpdateCategoryControllerFactory } from '@/main/factories/controllers/category'
 import { adminAuth } from '@/main/middlewares/account'
 
 export default (router: Router): void => {
   router.post('/category', adminAuth, ExpressRouteAdapter.adapt(AddCategoryControllerFactory.makeAddCategoryController()))
+  router.put('/category/:id', adminAuth, ExpressRouteAdapter.adapt(UpdateCategoryControllerFactory.makeUpdateCategoryController()))
   router.get('/category', ExpressRouteAdapter.adapt(GetAllCategoriesControllerFactory.makeGetAllCategoriesController()))
 }
