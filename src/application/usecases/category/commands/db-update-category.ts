cc9b0d9558bfc8073627e463872ebfd5facc7bf5
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-01-10 03:00:26 -0300

    feat: ensure DbUpdateCategory throws CategoryNotFoundError if CheckCategoryByIdRepository returns false

--- src/application/usecases/category/commands/db-update-category.ts
+++ src/application/usecases/category/commands/db-update-category.ts
@@ -1,6 +1,7 @@
 import { type CheckCategoryByIdRepository } from '@/application/protocols/category/queries'
 import { type UpdateCategoryRepository } from '@/application/protocols/category/commands'
 import { type UpdateCategory } from '@/domain/usecases/category/commands'
+import { CategoryNotFoundError } from '@/domain/errors/category'
 
 export class DbUpdateCategory implements UpdateCategory {
   constructor(
@@ -9,7 +10,10 @@ export class DbUpdateCategory implements UpdateCategory {
   ) {}
 
   public async update(input: UpdateCategory.Input): Promise<void> {
-    await this.checkCategoryByIdRepository.checkById(input.id)
+    const categoryExists = await this.checkCategoryByIdRepository.checkById(input.id)
+    if (!categoryExists) {
+      throw new CategoryNotFoundError()
+    }
     await this.updateCategoryRepository.update(input)
   }
 }
