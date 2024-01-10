739a222d63b7e3bd31d712edd0d1dec14801ec2c
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-01-10 01:57:39 -0300

    refactor: move sut creation to a factory

--- tests/application/mocks/category/commands/mock-db-category-commands.ts
+++ tests/application/mocks/category/commands/mock-db-category-commands.ts
@@ -1,9 +1,17 @@
-import { type AddCategoryRepository } from '@/application/protocols/category/commands'
+import { type AddCategoryRepository, type UpdateCategoryRepository } from '@/application/protocols/category/commands'
 
 export class AddCategoryRepositorySpy implements AddCategoryRepository {
   public input: AddCategoryRepository.Input
 
   public async add(input: AddCategoryRepository.Input): Promise<void> {
+    this.input = input
+  }
+}
+
+export class UpdateCategoryRepositorySpy implements UpdateCategoryRepository {
+  public input: UpdateCategoryRepository.Input
+
+  public async update(input: UpdateCategoryRepository.Input): Promise<void> {
     this.input = input
   }
 }
--- tests/application/usecases/category/commands/db-update-category.spec.ts
+++ tests/application/usecases/category/commands/db-update-category.spec.ts
@@ -1,18 +1,24 @@
+import { UpdateCategoryRepositorySpy } from '@/tests/application/mocks/category/commands'
 import { mockUpdateCategoryInput } from '@/tests/domain/mocks/category'
-import { type UpdateCategoryRepository } from '@/application/protocols/category/commands'
 import { DbUpdateCategory } from '@/application/usecases/category/commands'
+
+interface Sut {
+  sut: DbUpdateCategory
+  updateCategoryRepositorySpy: UpdateCategoryRepositorySpy
+}
+
+const makeSut = (): Sut => {
+  const updateCategoryRepositorySpy = new UpdateCategoryRepositorySpy()
+  const sut = new DbUpdateCategory(updateCategoryRepositorySpy)
+  return {
+    sut,
+    updateCategoryRepositorySpy
+  }
+}
 
 describe('DbUpdateCategory', () => {
   test('Should call UpdateCategoryRepository with correct values', async() => {
-    class UpdateCategoryRepositorySpy implements UpdateCategoryRepository {
-      public input: UpdateCategoryRepository.Input
-
-      public async update(input: UpdateCategoryRepository.Input): Promise<void> {
-        this.input = input
-      }
-    }
-    const updateCategoryRepositorySpy = new UpdateCategoryRepositorySpy()
-    const sut = new DbUpdateCategory(updateCategoryRepositorySpy)
+    const { sut, updateCategoryRepositorySpy } = makeSut()
     const updateCategoryInput = mockUpdateCategoryInput()
     await sut.update(updateCategoryInput)
     expect(updateCategoryRepositorySpy.input).toEqual(updateCategoryInput)
