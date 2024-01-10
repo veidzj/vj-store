3bb3d17e1c26b64062abc4d156b1637c880dbf1d
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-01-09 23:11:09 -0300

    feat: add GET /category route

---
+++ src/main/factories/controllers/category/get-all-categories-controller-factory.ts
@@ -0,0 +1,10 @@
+import { GetAllCategoriesFactory } from '@/main/factories/usecases/category/queries'
+import { type Controller } from '@/presentation/protocols'
+import { GetAllCategoriesController } from '@/presentation/controllers/category'
+
+export class GetAllCategoriesControllerFactory {
+  public static makeGetAllCategoriesController = (): Controller => {
+    const controller = new GetAllCategoriesController(GetAllCategoriesFactory.makeGetAllCategories())
+    return controller
+  }
+}
---
+++ src/main/factories/usecases/category/queries/get-all-categories-factory.ts
@@ -0,0 +1,10 @@
+import { type GetAllCategories } from '@/domain/usecases/category/queries'
+import { DbGetAllCategories } from '@/application/usecases/category/queries'
+import { GetAllCategoriesMongoRepository } from '@/infra/db/mongodb/category/queries'
+
+export class GetAllCategoriesFactory {
+  public static makeGetAllCategories = (): GetAllCategories => {
+    const getAllCategories = new GetAllCategoriesMongoRepository()
+    return new DbGetAllCategories(getAllCategories)
+  }
+}
