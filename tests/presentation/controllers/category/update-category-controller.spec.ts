44d3b3961865eb361d41c4a2506e027c459f4c54
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-01-10 00:51:36 -0300

    refactor: move sut creation to a factory

--- tests/presentation/controllers/category/update-category-controller.spec.ts
+++ tests/presentation/controllers/category/update-category-controller.spec.ts
@@ -1,20 +1,26 @@
+import { UpdateCategorySpy } from '@/tests/presentation/mocks/category'
 import { mockUpdateCategoryInput } from '@/tests/domain/mocks/category'
 import { UpdateCategoryController } from '@/presentation/controllers/category/update-category-controller'
-import { type UpdateCategory } from '@/domain/usecases/category/commands'
+
+interface Sut {
+  sut: UpdateCategoryController
+  updateCategorySpy: UpdateCategorySpy
+}
+
+const makeSut = (): Sut => {
+  const updateCategorySpy = new UpdateCategorySpy()
+  const sut = new UpdateCategoryController(updateCategorySpy)
+  return {
+    sut,
+    updateCategorySpy
+  }
+}
 
 const mockRequest = (): UpdateCategoryController.Request => mockUpdateCategoryInput()
 
 describe('UpdateCategoryController', () => {
   test('Should call UpdateCategory with correct values', async() => {
-    class UpdateCategorySpy implements UpdateCategory {
-      public input: UpdateCategory.Input
-
-      public async update(input: UpdateCategory.Input): Promise<void> {
-        this.input = input
-      }
-    }
-    const updateCategorySpy = new UpdateCategorySpy()
-    const sut = new UpdateCategoryController(updateCategorySpy)
+    const { sut, updateCategorySpy } = makeSut()
     const request = mockRequest()
     await sut.handle(request)
     expect(updateCategorySpy.input).toEqual(request)
--- tests/presentation/mocks/category/mock-category.ts
+++ tests/presentation/mocks/category/mock-category.ts
@@ -1,11 +1,19 @@
 import { mockCategoriesOutput } from '@/tests/domain/mocks/category'
-import { type AddCategory } from '@/domain/usecases/category/commands'
+import { type AddCategory, type UpdateCategory } from '@/domain/usecases/category/commands'
 import { type GetAllCategories } from '@/domain/usecases/category/queries'
 
 export class AddCategorySpy implements AddCategory {
   public input: AddCategory.Input
 
   public async add(input: AddCategory.Input): Promise<void> {
+    this.input = input
+  }
+}
+
+export class UpdateCategorySpy implements UpdateCategory {
+  public input: UpdateCategory.Input
+
+  public async update(input: UpdateCategory.Input): Promise<void> {
     this.input = input
   }
 }
