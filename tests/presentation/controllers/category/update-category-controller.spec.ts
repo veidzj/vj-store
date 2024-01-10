1a150f9ea5c2bddd998b336d88e604886774b2ac
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-01-10 00:55:06 -0300

    test: ensure UpdateCategoryController returns serverError if UpdateCategory throws

--- tests/presentation/controllers/category/update-category-controller.spec.ts
+++ tests/presentation/controllers/category/update-category-controller.spec.ts
@@ -1,6 +1,8 @@
+import { throwError } from '@/tests/test-helper'
 import { UpdateCategorySpy } from '@/tests/presentation/mocks/category'
 import { mockUpdateCategoryInput } from '@/tests/domain/mocks/category'
 import { UpdateCategoryController } from '@/presentation/controllers/category/update-category-controller'
+import { HttpHelper } from '@/presentation/helpers'
 
 interface Sut {
   sut: UpdateCategoryController
@@ -24,5 +26,13 @@ describe('UpdateCategoryController', () => {
     const request = mockRequest()
     await sut.handle(request)
     expect(updateCategorySpy.input).toEqual(request)
+  })
+
+  test('Should return serverError if UpdateCategory throws', async() => {
+    const { sut, updateCategorySpy } = makeSut()
+    jest.spyOn(updateCategorySpy, 'update').mockImplementationOnce(throwError)
+    const request = mockRequest()
+    const response = await sut.handle(request)
+    expect(response).toEqual(HttpHelper.serverError(new Error()))
   })
 })
