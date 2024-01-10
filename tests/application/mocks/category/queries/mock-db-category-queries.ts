d5733ce78571651fdc8520b7d27d39dddb57d1d4
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-01-09 22:00:16 -0300

    refactor: move sut creation to a factory

--- tests/application/mocks/category/queries/mock-db-category-queries.ts
+++ tests/application/mocks/category/queries/mock-db-category-queries.ts
@@ -1,4 +1,5 @@
-import { type CheckCategoryByNameRepository } from '@/application/protocols/category/queries'
+import { mockCategoriesOutput } from '@/tests/domain/mocks/category'
+import { type CheckCategoryByNameRepository, type GetAllCategoriesRepository } from '@/application/protocols/category/queries'
 
 export class CheckCategoryByNameRepositorySpy implements CheckCategoryByNameRepository {
   public name: string
@@ -6,5 +7,13 @@ export class CheckCategoryByNameRepositorySpy implements CheckCategoryByNameRepo
   public async checkByName(name: string): Promise<boolean> {
     this.name = name
     return false
+  }
+}
+
+export class GetAllCategoriesRepositorySpy implements GetAllCategoriesRepository {
+  public output: GetAllCategoriesRepository.Output[] = mockCategoriesOutput()
+
+  public async getAll(): Promise<GetAllCategoriesRepository.Output[]> {
+    return this.output
   }
 }
--- tests/application/usecases/category/queries/db-get-all-categories.spec.ts
+++ tests/application/usecases/category/queries/db-get-all-categories.spec.ts
@@ -1,18 +1,23 @@
+import { GetAllCategoriesRepositorySpy } from '@/tests/application/mocks/category/queries'
 import { DbGetAllCategories } from '@/application/usecases/category/queries'
-import { type GetAllCategoriesRepository } from '@/application/protocols/category/queries'
-import { mockCategoriesOutput } from '@/tests/domain/mocks/category'
+
+interface Sut {
+  sut: DbGetAllCategories
+  getAllCategoriesRepositorySpy: GetAllCategoriesRepositorySpy
+}
+
+const makeSut = (): Sut => {
+  const getAllCategoriesRepositorySpy = new GetAllCategoriesRepositorySpy()
+  const sut = new DbGetAllCategories(getAllCategoriesRepositorySpy)
+  return {
+    sut,
+    getAllCategoriesRepositorySpy
+  }
+}
 
 describe('DbGetAllCategories', () => {
   test('Should call GetAllCategoriesRepository', async() => {
-    class GetAllCategoriesRepositorySpy implements GetAllCategoriesRepository {
-      public output: GetAllCategoriesRepository.Output[] = mockCategoriesOutput()
-
-      public async getAll(): Promise<GetAllCategoriesRepository.Output[]> {
-        return this.output
-      }
-    }
-    const getAllCategoriesRepositorySpy = new GetAllCategoriesRepositorySpy()
-    const sut = new DbGetAllCategories(getAllCategoriesRepositorySpy)
+    const { sut, getAllCategoriesRepositorySpy } = makeSut()
     jest.spyOn(getAllCategoriesRepositorySpy, 'getAll')
     await sut.getAll()
     expect(getAllCategoriesRepositorySpy.getAll).toHaveBeenCalledTimes(1)
