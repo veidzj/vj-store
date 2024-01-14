6d12504f621bbfcb683629ba821801b7802f163e
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-01-14 11:59:11 -0300

    refactor: move sut creation to a factory

--- tests/application/usecases/product/commands/db-add-product.spec.ts
+++ tests/application/usecases/product/commands/db-add-product.spec.ts
@@ -3,20 +3,33 @@ import { mockAddProductInput } from '@/tests/domain/mocks/product'
 import { DbAddProduct } from '@/application/usecases/product/commands'
 import { CategoryNotFoundError } from '@/domain/errors/category'
 
+interface Sut {
+  sut: DbAddProduct
+  checkCategoryByNameRepositorySpy: CheckCategoryByNameRepositorySpy
+}
+
+const makeSut = (): Sut => {
+  const checkCategoryByNameRepositorySpy = new CheckCategoryByNameRepositorySpy()
+  checkCategoryByNameRepositorySpy.output = true
+  const sut = new DbAddProduct(checkCategoryByNameRepositorySpy)
+  return {
+    sut,
+    checkCategoryByNameRepositorySpy
+  }
+}
+
 describe('DbAddProduct', () => {
   describe('CheckCategoryByNameRepository', () => {
     test('Should call CheckCategoryByNameRepository with correct name', async() => {
-      const checkCategoryByNameRepositorySpy = new CheckCategoryByNameRepositorySpy()
-      checkCategoryByNameRepositorySpy.output = true
-      const sut = new DbAddProduct(checkCategoryByNameRepositorySpy)
+      const { sut, checkCategoryByNameRepositorySpy } = makeSut()
       const addProductInput = mockAddProductInput()
       await sut.add(addProductInput)
       expect(checkCategoryByNameRepositorySpy.name).toBe(addProductInput.category)
     })
 
     test('Should throw CategoryNotFoundError if CheckCategoryByNameRepository returns false', async() => {
-      const checkCategoryByNameRepositorySpy = new CheckCategoryByNameRepositorySpy()
-      const sut = new DbAddProduct(checkCategoryByNameRepositorySpy)
+      const { sut, checkCategoryByNameRepositorySpy } = makeSut()
+      checkCategoryByNameRepositorySpy.output = false
       const promise = sut.add(mockAddProductInput())
       await expect(promise).rejects.toThrow(new CategoryNotFoundError())
     })
