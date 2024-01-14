29dc486c20f7ef2573ede4ea93ee9a4bee34842b
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-01-14 13:12:12 -0300

    test: ensure DbAddProduct calls AddProductRepository with correct values

--- tests/application/usecases/product/commands/db-add-product.spec.ts
+++ tests/application/usecases/product/commands/db-add-product.spec.ts
@@ -1,25 +1,40 @@
+import MockDate from 'mockdate'
+
 import { throwError } from '@/tests/test-helper'
 import { CheckCategoryByNameRepositorySpy } from '@/tests/application/mocks/category/queries'
+import { AddProductRepositorySpy } from '@/tests/application/mocks/product/commands'
 import { mockAddProductInput } from '@/tests/domain/mocks/product'
 import { DbAddProduct } from '@/application/usecases/product/commands'
 import { CategoryNotFoundError } from '@/domain/errors/category'
+import { ProductHelper } from '@/domain/entities/product'
 
 interface Sut {
   sut: DbAddProduct
   checkCategoryByNameRepositorySpy: CheckCategoryByNameRepositorySpy
+  addProductRepositorySpy: AddProductRepositorySpy
 }
 
 const makeSut = (): Sut => {
   const checkCategoryByNameRepositorySpy = new CheckCategoryByNameRepositorySpy()
   checkCategoryByNameRepositorySpy.output = true
-  const sut = new DbAddProduct(checkCategoryByNameRepositorySpy)
+  const addProductRepositorySpy = new AddProductRepositorySpy()
+  const sut = new DbAddProduct(checkCategoryByNameRepositorySpy, addProductRepositorySpy)
   return {
     sut,
-    checkCategoryByNameRepositorySpy
+    checkCategoryByNameRepositorySpy,
+    addProductRepositorySpy
   }
 }
 
 describe('DbAddProduct', () => {
+  beforeAll(() => {
+    MockDate.set(new Date())
+  })
+
+  afterAll(() => {
+    MockDate.reset()
+  })
+
   describe('CheckCategoryByNameRepository', () => {
     test('Should call CheckCategoryByNameRepository with correct name', async() => {
       const { sut, checkCategoryByNameRepositorySpy } = makeSut()
@@ -40,6 +55,25 @@ describe('DbAddProduct', () => {
       jest.spyOn(checkCategoryByNameRepositorySpy, 'checkByName').mockImplementationOnce(throwError)
       const promise = sut.add(mockAddProductInput())
       await expect(promise).rejects.toThrow()
+    })
+  })
+
+  describe('AddProductRepository', () => {
+    test('Should call AddProductRepository with correct values', async() => {
+      const { sut, addProductRepositorySpy } = makeSut()
+      const addProductInput = mockAddProductInput()
+      await sut.add(addProductInput)
+      expect(addProductRepositorySpy.input.id).toBeTruthy()
+      expect(addProductRepositorySpy.input.name).toBe(addProductInput.name)
+      expect(addProductRepositorySpy.input.description).toBe(addProductInput.description)
+      expect(addProductRepositorySpy.input.price).toBe(addProductInput.price)
+      expect(addProductRepositorySpy.input.discountPercentage).toBe(addProductInput.discountPercentage)
+      expect(addProductRepositorySpy.input.quantity).toBe(addProductInput.quantity)
+      expect(addProductRepositorySpy.input.category).toBe(addProductInput.category)
+      expect(addProductRepositorySpy.input.slug).toBe(ProductHelper.generateSlug(addProductInput.name))
+      expect(addProductRepositorySpy.input.imagesUrls).toBe(addProductInput.imagesUrls)
+      expect(addProductRepositorySpy.input.createdAt).toEqual(new Date())
+      expect(addProductRepositorySpy.input.updateHistory).toEqual([])
     })
   })
 })
