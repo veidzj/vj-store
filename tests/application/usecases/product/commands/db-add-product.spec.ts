0d22e9ab29bb1f0b9ba1da6dad4427b550c702b3
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-01-14 11:58:00 -0300

    test: ensure DbAddProduct throws CategoryNotFoundError if CheckCategoryByNameRepository returns false

--- tests/application/usecases/product/commands/db-add-product.spec.ts
+++ tests/application/usecases/product/commands/db-add-product.spec.ts
@@ -1,15 +1,24 @@
 import { CheckCategoryByNameRepositorySpy } from '@/tests/application/mocks/category/queries'
-import { DbAddProduct } from '@/application/usecases/product/commands'
 import { mockAddProductInput } from '@/tests/domain/mocks/product'
+import { DbAddProduct } from '@/application/usecases/product/commands'
+import { CategoryNotFoundError } from '@/domain/errors/category'
 
 describe('DbAddProduct', () => {
-  describe('CheckCategoryByNameMongoRepository', () => {
-    test('Should call CheckCategoryByNameMongoRepository with correct name', async() => {
+  describe('CheckCategoryByNameRepository', () => {
+    test('Should call CheckCategoryByNameRepository with correct name', async() => {
       const checkCategoryByNameRepositorySpy = new CheckCategoryByNameRepositorySpy()
+      checkCategoryByNameRepositorySpy.output = true
       const sut = new DbAddProduct(checkCategoryByNameRepositorySpy)
       const addProductInput = mockAddProductInput()
       await sut.add(addProductInput)
       expect(checkCategoryByNameRepositorySpy.name).toBe(addProductInput.category)
+    })
+
+    test('Should throw CategoryNotFoundError if CheckCategoryByNameRepository returns false', async() => {
+      const checkCategoryByNameRepositorySpy = new CheckCategoryByNameRepositorySpy()
+      const sut = new DbAddProduct(checkCategoryByNameRepositorySpy)
+      const promise = sut.add(mockAddProductInput())
+      await expect(promise).rejects.toThrow(new CategoryNotFoundError())
     })
   })
 })
