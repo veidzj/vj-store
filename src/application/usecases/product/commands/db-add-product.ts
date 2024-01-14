61ad658892d6f39ad431bc9dcc54da5e1c6375bc
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-01-14 11:57:39 -0300

    feat: ensure DbAddProduct throws CategoryNotFoundError if CheckCategoryByNameRepository returns false

--- src/application/usecases/product/commands/db-add-product.ts
+++ src/application/usecases/product/commands/db-add-product.ts
@@ -1,10 +1,14 @@
 import { type CheckCategoryByNameRepository } from '@/application/protocols/category/queries'
 import { type AddProduct } from '@/domain/usecases/product/commands'
+import { CategoryNotFoundError } from '@/domain/errors/category'
 
 export class DbAddProduct implements AddProduct {
   constructor(private readonly checkCategoryByNameRepository: CheckCategoryByNameRepository) {}
 
   public async add(input: AddProduct.Input): Promise<void> {
-    await this.checkCategoryByNameRepository.checkByName(input.category)
+    const categoryExists = await this.checkCategoryByNameRepository.checkByName(input.category)
+    if (!categoryExists) {
+      throw new CategoryNotFoundError()
+    }
   }
 }
