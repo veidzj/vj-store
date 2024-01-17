d447be18039c389f6c60160fb5819605887e81a5
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-01-17 16:50:56 -0300

    feat: ensure UpdateProductController returns serverError if UpdateProduct throws

--- src/presentation/controllers/product/update-product-controller.ts
+++ src/presentation/controllers/product/update-product-controller.ts
@@ -1,10 +1,17 @@
+import { type Controller, type Response } from '@/presentation/protocols'
+import { HttpHelper } from '@/presentation/helpers'
 import { type UpdateProduct } from '@/domain/usecases/product/commands'
 
-export class UpdateProductController {
+export class UpdateProductController implements Controller {
   constructor(private readonly updateProduct: UpdateProduct) {}
 
-  public async handle(request: UpdateProductController.Request): Promise<void> {
-    await this.updateProduct.update(request)
+  public async handle(request: UpdateProductController.Request): Promise<Response> {
+    try {
+      await this.updateProduct.update(request)
+      return HttpHelper.ok({})
+    } catch (error) {
+      return HttpHelper.serverError(error as Error)
+    }
   }
 }
 
