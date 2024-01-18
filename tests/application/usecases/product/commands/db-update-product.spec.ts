e08e262c73f1be7a50b53326b3f2b3aaa561ddda
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-01-18 00:24:02 -0300

    refactor: move sut creation to a factory

--- tests/application/usecases/product/commands/db-update-product.spec.ts
+++ tests/application/usecases/product/commands/db-update-product.spec.ts
@@ -1,21 +1,25 @@
+import { CheckProductByIdRepositorySpy } from '@/tests/application/mocks/product/queries'
 import { mockUpdateProductInput } from '@/tests/domain/mocks/product'
 import { DbUpdateProduct } from '@/application/usecases/product/commands'
-import { type CheckProductByIdRepository } from '@/application/protocols/product/queries'
+
+interface Sut {
+  sut: DbUpdateProduct
+  checkProductByIdRepositorySpy: CheckProductByIdRepositorySpy
+}
+
+const makeSut = (): Sut => {
+  const checkProductByIdRepositorySpy = new CheckProductByIdRepositorySpy()
+  const sut = new DbUpdateProduct(checkProductByIdRepositorySpy)
+  return {
+    sut,
+    checkProductByIdRepositorySpy
+  }
+}
 
 describe('DbUpdateProduct', () => {
   describe('CheckProductByIdRepository', () => {
     test('Should call CheckProductByIdRepository with correct id', async() => {
-      class CheckProductByIdRepositorySpy implements CheckProductByIdRepository {
-        public id: string
-        public output: boolean = true
-
-        public async checkById(id: string): Promise<boolean> {
-          this.id = id
-          return this.output
-        }
-      }
-      const checkProductByIdRepositorySpy = new CheckProductByIdRepositorySpy()
-      const sut = new DbUpdateProduct(checkProductByIdRepositorySpy)
+      const { sut, checkProductByIdRepositorySpy } = makeSut()
       const updateProductInput = mockUpdateProductInput()
       await sut.update(updateProductInput)
       expect(checkProductByIdRepositorySpy.id).toBe(updateProductInput.id)
