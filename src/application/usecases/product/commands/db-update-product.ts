aaeaf482ef26bad68e825785f28cc18337fa0d14
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-01-18 22:45:54 -0300

    feat: ensure DbUpdateProduct throws ProductNotFoundError if CheckProductByIdRepository returns false

--- src/application/usecases/product/commands/db-update-product.ts
+++ src/application/usecases/product/commands/db-update-product.ts
@@ -1,10 +1,14 @@
 import { type CheckProductByIdRepository } from '@/application/protocols/product/queries'
 import { type UpdateProduct } from '@/domain/usecases/product/commands'
+import { ProductNotFoundError } from '@/domain/errors/product'
 
 export class DbUpdateProduct implements UpdateProduct {
   constructor(private readonly checkProductByIdRepository: CheckProductByIdRepository) {}
 
   public async update(input: UpdateProduct.Input): Promise<void> {
-    await this.checkProductByIdRepository.checkById(input.id)
+    const productExists = await this.checkProductByIdRepository.checkById(input.id)
+    if (!productExists) {
+      throw new ProductNotFoundError()
+    }
   }
 }
