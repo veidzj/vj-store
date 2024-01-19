8963024ae08aaba99d2d094e6b3ece4645166f69
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-01-19 03:37:32 -0300

    feat: ensure DbUpdateProduct calls CheckCategoryByNameRepository with correct name

--- src/application/usecases/product/commands/db-update-product.ts
+++ src/application/usecases/product/commands/db-update-product.ts
@@ -1,14 +1,19 @@
 import { type CheckProductByIdRepository } from '@/application/protocols/product/queries'
+import { type CheckCategoryByNameRepository } from '@/application/protocols/category/queries'
 import { type UpdateProduct } from '@/domain/usecases/product/commands'
 import { ProductNotFoundError } from '@/domain/errors/product'
 
 export class DbUpdateProduct implements UpdateProduct {
-  constructor(private readonly checkProductByIdRepository: CheckProductByIdRepository) {}
+  constructor(
+    private readonly checkProductByIdRepository: CheckProductByIdRepository,
+    private readonly checkCategoryByNameRepository: CheckCategoryByNameRepository
+  ) {}
 
   public async update(input: UpdateProduct.Input): Promise<void> {
     const productExists = await this.checkProductByIdRepository.checkById(input.id)
     if (!productExists) {
       throw new ProductNotFoundError()
     }
+    await this.checkCategoryByNameRepository.checkByName(input.category)
   }
 }
