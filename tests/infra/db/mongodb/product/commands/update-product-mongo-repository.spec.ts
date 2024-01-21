53049567630e86dcb56ca8c9baf3874b25dcb66d
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-01-21 18:29:37 -0300

    test: ensure UpdateProductMongoRepository updates a product on success

--- tests/infra/db/mongodb/product/commands/update-product-mongo-repository.spec.ts
+++ tests/infra/db/mongodb/product/commands/update-product-mongo-repository.spec.ts
@@ -3,7 +3,7 @@ import { Collection } from 'mongodb'
 import { throwError } from '@/tests/test-helper'
 import { connectToDatabase, disconnectFromDatabase, clearCollection } from '@/tests/infra/db/mongodb'
 import { getProductCollection } from '@/tests/infra/db/mongodb/product'
-import { mockUpdateProductRepositoryInput } from '@/tests/application/mocks/product/commands'
+import { mockAddProductRepositoryInput, mockUpdateProductRepositoryInput } from '@/tests/application/mocks/product/commands'
 import { UpdateProductMongoRepository } from '@/infra/db/mongodb/product/commands'
 
 let productCollection: Collection
@@ -32,5 +32,25 @@ describe('UpdateProductMongoRepository', () => {
     jest.spyOn(Collection.prototype, 'updateOne').mockImplementationOnce(throwError)
     const promise = sut.update(mockUpdateProductRepositoryInput())
     await expect(promise).rejects.toThrow()
+  })
+
+  test('Should update a product on success', async() => {
+    const sut = makeSut()
+    const addProductRepositoryInput = mockAddProductRepositoryInput()
+    await productCollection.insertOne(addProductRepositoryInput)
+    const updateProductRepositoryInput = { ...mockUpdateProductRepositoryInput(), id: addProductRepositoryInput.id }
+    await sut.update(updateProductRepositoryInput)
+    const updatedProduct = await productCollection.findOne({ id: addProductRepositoryInput.id })
+    expect(updatedProduct?.id).toBe(updateProductRepositoryInput.id)
+    expect(updatedProduct?.name).toBe(updateProductRepositoryInput.name)
+    expect(updatedProduct?.description).toBe(updateProductRepositoryInput.description)
+    expect(updatedProduct?.price).toBe(updateProductRepositoryInput.price)
+    expect(updatedProduct?.discountPercentage).toBe(updateProductRepositoryInput.discountPercentage)
+    expect(updatedProduct?.quantity).toBe(updateProductRepositoryInput.quantity)
+    expect(updatedProduct?.category).toBe(updateProductRepositoryInput.category)
+    expect(updatedProduct?.slug).toBe(updateProductRepositoryInput.slug)
+    expect(updatedProduct?.imagesUrls).toEqual(updateProductRepositoryInput.imagesUrls)
+    expect(updatedProduct?.createdAt).toEqual(addProductRepositoryInput.createdAt)
+    expect(updatedProduct?.updatedAt).toEqual(updateProductRepositoryInput.updatedAt)
   })
 })
