db60fb2b4b7b5ce0217e26ad6c3bced6a382ecc5
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-01-24 16:21:33 -0300

    test: ensure GetLatestProductsController calls GetLatestProducts with given pagination

--- tests/presentation/controllers/product/queries/get-latest-products-controller.spec.ts
+++ tests/presentation/controllers/product/queries/get-latest-products-controller.spec.ts
@@ -1,3 +1,5 @@
+import { faker } from '@faker-js/faker'
+
 import { GetLatestProductsSpy } from '@/tests/presentation/mocks/product'
 import { GetLatestProductsController } from '@/presentation/controllers/product/queries'
 import { DEFAULT_PAGE, DEFAULT_LIMIT } from '@/presentation/constants'
@@ -16,13 +18,30 @@ const makeSut = (): Sut => {
   }
 }
 
+const mockRequestWithoutPagination = (): GetLatestProductsController.Request => ({})
+
+const mockRequestWithPagination = (): GetLatestProductsController.Request => ({
+  page: faker.number.int().toString(),
+  limit: faker.number.int().toString()
+})
+
 describe('GetLatestProductsController', () => {
   test('Should call GetLatestProducts with default pagination', async() => {
     const { sut, getLatestProductsSpy } = makeSut()
     jest.spyOn(getLatestProductsSpy, 'getLatest')
-    await sut.handle({})
+    await sut.handle(mockRequestWithoutPagination())
     expect(getLatestProductsSpy.getLatest).toHaveBeenCalledTimes(1)
     expect(getLatestProductsSpy.page).toBe(DEFAULT_PAGE)
     expect(getLatestProductsSpy.limit).toBe(DEFAULT_LIMIT)
+  })
+
+  test('Should call GetLatestProducts with given pagination', async() => {
+    const { sut, getLatestProductsSpy } = makeSut()
+    jest.spyOn(getLatestProductsSpy, 'getLatest')
+    const request = mockRequestWithPagination()
+    await sut.handle(request)
+    expect(getLatestProductsSpy.getLatest).toHaveBeenCalledTimes(1)
+    expect(getLatestProductsSpy.page).toBe(Number(request.page))
+    expect(getLatestProductsSpy.limit).toBe(Number(request.limit))
   })
 })
