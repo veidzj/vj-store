6cf995266cc213abf15c37b4b6c913c92e05e862
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-01-24 02:20:53 -0300

    test: ensure GetLatestProductsController calls GetLatestProducts with default pagination

--- tests/presentation/controllers/product/queries/get-latest-products-controller.spec.ts
+++ tests/presentation/controllers/product/queries/get-latest-products-controller.spec.ts
@@ -1,19 +1,26 @@
 import { GetLatestProductsController } from '@/presentation/controllers/product/queries'
 import { type GetLatestProducts } from '@/domain/usecases/product/queries'
+import { DEFAULT_PAGE, DEFAULT_LIMIT } from '@/presentation/constants'
 
 describe('GetLatestProductsController', () => {
-  test('Should call GetLatestProducts', async() => {
+  test('Should call GetLatestProducts with default pagination', async() => {
     class GetLatestProductsSpy implements GetLatestProducts {
+      public page: number
+      public limit: number
       public output: GetLatestProducts.Output
 
-      public async getLatest(): Promise<GetLatestProducts.Output> {
+      public async getLatest(page: number, limit: number): Promise<GetLatestProducts.Output> {
+        this.page = page
+        this.limit = limit
         return this.output
       }
     }
     const getLatestProductsSpy = new GetLatestProductsSpy()
     const sut = new GetLatestProductsController(getLatestProductsSpy)
     jest.spyOn(getLatestProductsSpy, 'getLatest')
-    await sut.handle()
+    await sut.handle({})
     expect(getLatestProductsSpy.getLatest).toHaveBeenCalledTimes(1)
+    expect(getLatestProductsSpy.page).toBe(DEFAULT_PAGE)
+    expect(getLatestProductsSpy.limit).toBe(DEFAULT_LIMIT)
   })
 })
