aee34d81995383ecafa8ae9f9121308654500b7f
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-01-26 01:52:34 -0300

    test: ensure GetLatestProductsMongoRepository returns all products ordered by most recent with pagination values

--- tests/infra/db/mongodb/product/queries/get-latest-products-mongo-repository.spec.ts
+++ tests/infra/db/mongodb/product/queries/get-latest-products-mongo-repository.spec.ts
@@ -13,8 +13,9 @@ const makeSut = (): GetLatestProductsMongoRepository => {
 }
 
 const defaultLength: number = 30
-const randomDate: Date = faker.date.anytime()
 const defaultPage: number = 1
+const randomDate: Date = faker.date.anytime()
+const randomPage: number = faker.number.int({ min: 1, max: 5 })
 const randomLimit: number = faker.number.int({ min: 5, max: 25 })
 
 describe('GetLatestProductsMongoRepository', () => {
@@ -33,16 +34,19 @@ describe('GetLatestProductsMongoRepository', () => {
     await clearCollection(productCollection)
   })
 
-  test('Should return all products ordered by most recent', async() => {
+  test('Should return all products ordered by most recent with pagination values', async() => {
     const addProductsRepositoryInput = Array.from({ length: defaultLength }, () => {
       const addProductRepositoryInput = mockAddProductRepositoryInput()
       return { ...addProductRepositoryInput, createdAt: randomDate }
     })
     await productCollection.insertMany(addProductsRepositoryInput)
-    const { products } = await sut.getLatest(1, 25)
+    const { products, currentPage, totalPages, totalItems } = await sut.getLatest(randomPage, randomLimit)
     for (let i = 0; i < products.length - 1; i++) {
       expect(products[i].createdAt.getTime()).toBeGreaterThanOrEqual(products[i + 1].createdAt.getTime())
     }
+    expect(currentPage).toBe(randomPage)
+    expect(totalItems).toBe(defaultLength)
+    expect(totalPages).toBe(Math.ceil(totalItems / randomLimit))
   })
 
   test('Should return only the limit of products of pagination', async() => {
