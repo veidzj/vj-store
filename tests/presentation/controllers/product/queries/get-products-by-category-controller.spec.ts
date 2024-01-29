bfb282074c773a73963b9546fe483e87a426ae1f
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-01-28 22:19:29 -0300

    test: ensure GetProductsByCategoryController calls GetProductsByCategory with default pagination

---
+++ tests/presentation/controllers/product/queries/get-products-by-category-controller.spec.ts
@@ -0,0 +1,30 @@
+import { GetProductsByCategorySpy } from '@/tests/presentation/mocks/product'
+import { GetProductsByCategoryController } from '@/presentation/controllers/product/queries'
+import { DEFAULT_PAGE, DEFAULT_LIMIT } from '@/presentation/constants'
+
+interface Sut {
+  sut: GetProductsByCategoryController
+  getProductsByCategorySpy: GetProductsByCategorySpy
+}
+
+const makeSut = (): Sut => {
+  const getProductsByCategorySpy = new GetProductsByCategorySpy()
+  const sut = new GetProductsByCategoryController(getProductsByCategorySpy)
+  return {
+    sut,
+    getProductsByCategorySpy
+  }
+}
+
+const mockRequestWithoutPagination = (): GetProductsByCategoryController.Request => ({})
+
+describe('GetProductsByCategoryController', () => {
+  test('Should call GetProductsByCategory with default pagination', async() => {
+    const { sut, getProductsByCategorySpy } = makeSut()
+    jest.spyOn(getProductsByCategorySpy, 'getByCategory')
+    await sut.handle(mockRequestWithoutPagination())
+    expect(getProductsByCategorySpy.getByCategory).toHaveBeenCalledTimes(1)
+    expect(getProductsByCategorySpy.page).toBe(DEFAULT_PAGE)
+    expect(getProductsByCategorySpy.limit).toBe(DEFAULT_LIMIT)
+  })
+})
