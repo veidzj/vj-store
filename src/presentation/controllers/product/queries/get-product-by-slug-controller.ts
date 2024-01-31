92239a632a58a3ecaa5785b6de39c5fc38b162bd
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-01-30 21:46:21 -0300

    feat: ensure GetProductBySlugController returns notFound if GetProductBySlug throws ProductNotFoundError

--- src/presentation/controllers/product/queries/get-product-by-slug-controller.ts
+++ src/presentation/controllers/product/queries/get-product-by-slug-controller.ts
@@ -1,6 +1,7 @@
 import { type Controller, type Response } from '@/presentation/protocols'
 import { HttpHelper } from '@/presentation/helpers'
 import { type GetProductBySlug } from '@/domain/usecases/product/queries'
+import { ProductNotFoundError } from '@/domain/errors/product'
 
 export class GetProductBySlugController implements Controller {
   constructor(private readonly getProductBySlug: GetProductBySlug) {}
@@ -10,6 +11,9 @@ export class GetProductBySlugController implements Controller {
       await this.getProductBySlug.getBySlug(request.slug)
       return HttpHelper.ok({})
     } catch (error) {
+      if (error instanceof ProductNotFoundError) {
+        return HttpHelper.notFound(error)
+      }
       return HttpHelper.serverError(error as Error)
     }
   }
