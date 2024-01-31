13b2492c1170cbebbcd021460c6bdfdea98f02b6
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-01-31 15:38:12 -0300

    feat: ensure DbGetProductBySlug returns a product on success

--- src/application/usecases/product/queries/db-get-product-by-slug.ts
+++ src/application/usecases/product/queries/db-get-product-by-slug.ts
@@ -1,17 +1,20 @@
 import { type CheckProductBySlugRepository, type GetProductBySlugRepository } from '@/application/protocols/product/queries'
+import { type ProductOutput } from '@/domain/entities/product/dto'
+import { type GetProductBySlug } from '@/domain/usecases/product/queries'
 import { ProductNotFoundError } from '@/domain/errors/product'
 
-export class DbGetProductBySlug {
+export class DbGetProductBySlug implements GetProductBySlug {
   constructor(
     private readonly checkProductBySlugRepository: CheckProductBySlugRepository,
     private readonly getProductBySlugRepository: GetProductBySlugRepository
   ) {}
 
-  public async getBySlug(slug: string): Promise<void> {
+  public async getBySlug(slug: string): Promise<ProductOutput> {
     const productExists = await this.checkProductBySlugRepository.checkBySlug(slug)
     if (!productExists) {
       throw new ProductNotFoundError()
     }
-    await this.getProductBySlugRepository.getBySlug(slug)
+    const product = await this.getProductBySlugRepository.getBySlug(slug)
+    return product
   }
 }
