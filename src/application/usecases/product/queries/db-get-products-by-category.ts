fe90d283b6ddbfe21fbb3f6233365c228068bbbf
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-01-31 06:17:12 -0300

    feat: ensure DbGetProductsByCategory calls CheckCategoryByNameRepository with correct value

--- src/application/usecases/product/queries/db-get-products-by-category.ts
+++ src/application/usecases/product/queries/db-get-products-by-category.ts
@@ -1,11 +1,16 @@
+import { type CheckCategoryByNameRepository } from '@/application/protocols/category/queries'
 import { type GetProductsByCategoryRepository } from '@/application/protocols/product/queries'
 import { type GetProductsByCategory } from '@/domain/usecases/product/queries'
 import { type ProductsOutput } from '@/domain/entities/product/dto'
 
 export class DbGetProductsByCategory implements GetProductsByCategory {
-  constructor(private readonly getProductsByCategoryRepository: GetProductsByCategoryRepository) {}
+  constructor(
+    private readonly checkCategoryByNameRepository: CheckCategoryByNameRepository,
+    private readonly getProductsByCategoryRepository: GetProductsByCategoryRepository
+  ) {}
 
   public async getByCategory(category: string, page: number, limit: number): Promise<ProductsOutput> {
+    await this.checkCategoryByNameRepository.checkByName(category)
     const { products, currentPage, totalPages, totalItems } = await this.getProductsByCategoryRepository.getByCategory(category, page, limit)
     return {
       products,
