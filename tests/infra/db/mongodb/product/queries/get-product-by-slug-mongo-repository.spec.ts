57cb4960e3d94f9a1dc6bbc700b5d517c6365fab
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-01-31 23:46:43 -0300

    test: ensure GetProductBySlugMongoRepository returns a product on success

---
+++ tests/infra/db/mongodb/product/queries/get-product-by-slug-mongo-repository.spec.ts
@@ -0,0 +1,44 @@
+import { type Collection } from 'mongodb'
+
+import { connectToDatabase, disconnectFromDatabase, clearCollection } from '@/tests/infra/db/mongodb'
+import { getProductCollection } from '@/tests/infra/db/mongodb/product'
+import { mockAddProductRepositoryInput } from '@/tests/application/mocks/product/commands'
+import { GetProductBySlugMongoRepository } from '@/infra/db/mongodb/product/queries'
+
+let productCollection: Collection
+
+const makeSut = (): GetProductBySlugMongoRepository => {
+  return new GetProductBySlugMongoRepository()
+}
+
+describe('GetProductBySlugMongoRepository', () => {
+  beforeAll(async() => {
+    await connectToDatabase()
+  })
+
+  afterAll(async() => {
+    await disconnectFromDatabase()
+  })
+
+  beforeEach(async() => {
+    productCollection = await getProductCollection()
+    await clearCollection(productCollection)
+  })
+
+  test('Should return a product on success', async() => {
+    const addProductRepositoryInput = mockAddProductRepositoryInput()
+    await productCollection.insertOne(addProductRepositoryInput)
+    const sut = makeSut()
+    const product = await sut.getBySlug(addProductRepositoryInput.slug)
+    expect(product?.id).toBe(addProductRepositoryInput.id)
+    expect(product?.name).toBe(addProductRepositoryInput.name)
+    expect(product?.description).toBe(addProductRepositoryInput.description)
+    expect(product?.price).toBe(addProductRepositoryInput.price)
+    expect(product?.discountPercentage).toBe(addProductRepositoryInput.discountPercentage)
+    expect(product?.quantity).toBe(addProductRepositoryInput.quantity)
+    expect(product?.category).toBe(addProductRepositoryInput.category)
+    expect(product?.slug).toBe(addProductRepositoryInput.slug)
+    expect(product?.imagesUrls).toEqual(addProductRepositoryInput.imagesUrls)
+    expect(product?.createdAt).toEqual(addProductRepositoryInput.createdAt)
+  })
+})
