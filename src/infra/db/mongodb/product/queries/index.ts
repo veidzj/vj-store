a164fea68dd5ac25419dcc55b5a3e7ae41df8b2e
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-01-31 21:06:57 -0300

    feat: ensure CheckProductBySlugMongoRepository throws if mongo throws

--- src/infra/db/mongodb/product/queries/index.ts
+++ src/infra/db/mongodb/product/queries/index.ts
@@ -1,5 +1,6 @@
 export * from './check-product-by-id-mongo-repository'
 export * from './check-product-by-name-mongo-repository'
+export * from './check-product-by-slug-mongo-repository'
 export * from './get-latest-products-mongo-repository'
 export * from './get-products-by-category-mongo-repository'
 export * from './get-products-with-discount-mongo-repository'
