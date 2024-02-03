3fb4c9c1fa58f516e047bb4ad84b8ad3a8de1b11
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-02-03 08:20:52 -0300

    feat: ensure ChangeEmailController returns serverError if ChangeEmail throws

--- src/presentation/controllers/account/commands/change-email-controller.ts
+++ src/presentation/controllers/account/commands/change-email-controller.ts
@@ -1,10 +1,17 @@
+import { type Controller, type Response } from '@/presentation/protocols'
+import { HttpHelper } from '@/presentation/helpers'
 import { type ChangeEmail } from '@/domain/usecases/account/commands'
 
-export class ChangeEmailController {
+export class ChangeEmailController implements Controller {
   constructor(private readonly changeEmail: ChangeEmail) {}
 
-  public async handle(request: ChangeEmailController.Request): Promise<void> {
-    await this.changeEmail.change(request.currentEmail, request.newEmail)
+  public async handle(request: ChangeEmailController.Request): Promise<Response> {
+    try {
+      await this.changeEmail.change(request.currentEmail, request.newEmail)
+      return HttpHelper.ok({})
+    } catch (error) {
+      return HttpHelper.serverError(error as Error)
+    }
   }
 }
 
