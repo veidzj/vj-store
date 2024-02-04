dc6a7221f5cf1615967443f0fdd48eb7f1669853
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-02-03 23:23:47 -0300

    feat: ensure DbChangeEmail calls CheckAccountByEmailRepository with correct email

--- src/application/usecases/account/commands/db-change-email.ts
+++ src/application/usecases/account/commands/db-change-email.ts
@@ -1,13 +1,22 @@
+import { type CheckAccountByEmailRepository } from '@/application/protocols/account/queries'
 import { type ChangeEmailRepository } from '@/application/protocols/account/commands'
 import { type ChangeEmail } from '@/domain/usecases/account/commands'
 import { AccountValidation } from '@/domain/entities/account'
+import { AccountNotFoundError } from '@/domain/errors/account'
 
 export class DbChangeEmail implements ChangeEmail {
-  constructor(private readonly changeEmailRepository: ChangeEmailRepository) {}
+  constructor(
+    private readonly checkAccountByEmailRepository: CheckAccountByEmailRepository,
+    private readonly changeEmailRepository: ChangeEmailRepository
+  ) {}
 
   public async change(currentEmail: string, newEmail: string): Promise<void> {
     AccountValidation.validateEmail(currentEmail)
     AccountValidation.validateEmail(newEmail)
+    const accountExists = await this.checkAccountByEmailRepository.checkByEmail(currentEmail)
+    if (!accountExists) {
+      throw new AccountNotFoundError()
+    }
     await this.changeEmailRepository.change(currentEmail, newEmail)
   }
 }
