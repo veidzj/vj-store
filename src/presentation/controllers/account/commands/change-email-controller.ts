f3534397eadc1cc1415ebcf6624e04c282a6700c
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-02-05 10:15:40 -0300

    feat: ensure ChangeEmailController returns unauthorized if currentEmail is not equal to accountEmail

--- src/presentation/controllers/account/commands/change-email-controller.ts
+++ src/presentation/controllers/account/commands/change-email-controller.ts
@@ -2,13 +2,16 @@ import { type Controller, type Response } from '@/presentation/protocols'
 import { HttpHelper } from '@/presentation/helpers'
 import { type ChangeEmail } from '@/domain/usecases/account/commands'
 import { EntityValidationError } from '@/domain/errors'
-import { AccountNotFoundError } from '@/domain/errors/account'
+import { AccountNotFoundError, InvalidCredentialsError } from '@/domain/errors/account'
 
 export class ChangeEmailController implements Controller {
   constructor(private readonly changeEmail: ChangeEmail) {}
 
   public async handle(request: ChangeEmailController.Request): Promise<Response> {
     try {
+      if (request.currentEmail !== request.accountEmail) {
+        return HttpHelper.unauthorized(new InvalidCredentialsError())
+      }
       await this.changeEmail.change(request.currentEmail, request.newEmail)
       return HttpHelper.ok({ message: 'Email successfully changed' })
     } catch (error) {
@@ -27,6 +30,6 @@ export namespace ChangeEmailController {
   export interface Request {
     currentEmail: string
     newEmail: string
-    accountId: string
+    accountEmail: string
   }
 }
