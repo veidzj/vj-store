914553deaf7f8c3939c1285584fb37bb3e5aa323
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-02-05 12:34:51 -0300

    feat: ensure ChangeAccountPasswordController returns serverError if ChangeAccountPassword throws

--- src/presentation/controllers/account/commands/change-account-password-controller.ts
+++ src/presentation/controllers/account/commands/change-account-password-controller.ts
@@ -1,10 +1,17 @@
 import { type ChangeAccountPassword } from '@/domain/usecases/account/commands'
+import { type Controller, type Response } from '@/presentation/protocols'
+import { HttpHelper } from '@/presentation/helpers'
 
-export class ChangeAccountPasswordController {
+export class ChangeAccountPasswordController implements Controller {
   constructor(private readonly changeAccountPassword: ChangeAccountPassword) {}
 
-  public async handle(request: ChangeAccountPasswordController.Request): Promise<void> {
-    await this.changeAccountPassword.changePassword(request.accountEmail, request.currentPassword, request.newPassword)
+  public async handle(request: ChangeAccountPasswordController.Request): Promise<Response> {
+    try {
+      await this.changeAccountPassword.changePassword(request.accountEmail, request.currentPassword, request.newPassword)
+      return HttpHelper.ok({})
+    } catch (error) {
+      return HttpHelper.serverError(error as Error)
+    }
   }
 }
 
