1d13976d66f930dd2139e994415a39c551a51278
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-02-05 05:05:20 -0300

    refactor: change adminAuth to userAuth in change-email route

--- src/main/routes/account-route.ts
+++ src/main/routes/account-route.ts
@@ -1,12 +1,12 @@
 import { type Router } from 'express'
 
 import { ExpressRouteAdapter } from '@/main/adapters'
-import { adminAuth } from '@/main/middlewares/account'
+import { userAuth } from '@/main/middlewares/account'
 import { SignUpControllerFactory, SignInControllerFactory } from '@/main/factories/controllers/account/authentication'
 import { ChangeEmailControllerFactory } from '@/main/factories/controllers/account/commands'
 
 export default (router: Router): void => {
   router.post('/account/signup', ExpressRouteAdapter.adapt(SignUpControllerFactory.makeSignUpController()))
   router.post('/account/signin', ExpressRouteAdapter.adapt(SignInControllerFactory.makeSignInController()))
-  router.put('/account/change-email', adminAuth, ExpressRouteAdapter.adapt(ChangeEmailControllerFactory.makeChangeEmailController()))
+  router.put('/account/change-email', userAuth, ExpressRouteAdapter.adapt(ChangeEmailControllerFactory.makeChangeEmailController()))
 }
