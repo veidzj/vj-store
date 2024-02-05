71032ab2b1524b78bce8afb3e725d6e219ed4305
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-02-05 12:39:19 -0300

    feat: ensure ChangeAccountPasswordController returns unauthorized if ChangeAccountPassword throws InvalidCredentialsError

--- src/presentation/controllers/account/commands/change-account-password-controller.ts
+++ src/presentation/controllers/account/commands/change-account-password-controller.ts
@@ -2,6 +2,7 @@ import { type ChangeAccountPassword } from '@/domain/usecases/account/commands'
 import { type Controller, type Response } from '@/presentation/protocols'
 import { HttpHelper } from '@/presentation/helpers'
 import { EntityValidationError } from '@/domain/errors'
+import { InvalidCredentialsError } from '@/domain/errors/account'
 
 export class ChangeAccountPasswordController implements Controller {
   constructor(private readonly changeAccountPassword: ChangeAccountPassword) {}
@@ -13,6 +14,9 @@ export class ChangeAccountPasswordController implements Controller {
     } catch (error) {
       if (error instanceof EntityValidationError) {
         return HttpHelper.badRequest(error)
+      }
+      if (error instanceof InvalidCredentialsError) {
+        return HttpHelper.unauthorized(error)
       }
       return HttpHelper.serverError(error as Error)
     }
